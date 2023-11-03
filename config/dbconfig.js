eb0b2a1767d72c96b421c524196f49b98b8f6b00
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-03 12:57:42 +0530

    db config updated

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
   try {
-    let db = await mongoose.connect('mongodb://' + process.env.HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASE, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // let db = await mongoose.connect('mongodb://' + process.env.HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_DATABASE, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
 
-    global.dbUrl = db.connections[0].db;
-    console.log('Database connected successfully');
+    await mongoose.connect(process.env.MONGO_ATLAS);
+
+    console.log("Database connected successfully!");
+
+    // global.dbUrl = db.connections[0].db;
+    // console.log('Database connected successfully');
   } catch (error) {
     console.error(error);
   }
--- models/service_master.model.js
+++ models/service_master.model.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const ServiceSchema = new Schema({
     parentId: { type: Schema.Types.ObjectId, ref: 'service_category', default: null, index: true },
-    title: { type: String, index: true }
+    title: { type: String, index: true },
+    order: { type: Number, index: true }
 }, { timestamps: true, versionKey: false });
 
 module.exports = mongoose.model('service_category', ServiceSchema);
