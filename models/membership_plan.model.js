375ef07089baa569758a242ba58398b968f8bb07
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-14 10:55:32 +0530

    updated

--- models/membership_plan.model.js
+++ models/membership_plan.model.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const MembershipPlanSchema = new Schema({
     title: { type: String, index: true, trim: true },
     amount: { type: Number, default: 0, trim: true },
+    status: { type: String, index: true, trim: true },
     no_of_months: { type: Number, default: 0, trim: true }
 }, { timestamps: true, versionKey: false });
 
