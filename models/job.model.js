6ef2d630d198d6712d05996bc1f6afe07f58bdf1
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-17 13:15:05 +0530

    job model updated

--- models/job.model.js
+++ models/job.model.js
@@ -5,16 +5,17 @@ const JobSchema = new mongoose.Schema({
     client_id: { type: Schema.Types.ObjectId, ref: 'User', index: true },
     title: { type: String, index: true, trim: true },
     description: { type: String, index: true, trim: true },
-    budget: { type: Number, trim: true },
     experience: { type: String, index: true, trim: true },
     skills: [{ type: String, index: true }],
     location: { type: String, index: true },
     role: { type: String, trim: true },
     role_category: { type: String, trim: true },
-    joining_type: { type: String, trim: true },
+    joining: { type: String },
     employment_type: { type: String, trim: true },
+    industry_type: { type: String, trim: true },
     department: { type: String, index: true },
-    date: { type: Date, default: Date.now(), index: true },
+    budget: { type: Number, trim: true },
+    date: { type: Date, index: true },
     status: { type: String, default: 'Open', enum: ['Open', 'Ongoing', 'Completed'] }
 }, { timestamps: true, versionKey: false });
 
