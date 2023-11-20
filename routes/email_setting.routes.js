bbdd29c746a95959354be70f54ebb5558ce98036
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-20 12:03:07 +0530

    email setting route updated"

--- routes/email_setting.routes.js
+++ routes/email_setting.routes.js
@@ -7,5 +7,6 @@ const request_param = multer();
 
 router.get('/admin/email/setting/details', Authentication.AuthenticateAdmin, emailSettingController.details);
 router.post('/admin/email/setting/add', request_param.any(), Authentication.AuthenticateAdmin, emailSettingController.add);
+router.post('/admin/email/setting/update/:id', request_param.any(), Authentication.AuthenticateAdmin, emailSettingController.update);
 
 module.exports = router;
