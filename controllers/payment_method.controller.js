9c6a1397d5f4b087ed12ccd362f8463b460143e7
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-20 15:07:42 +0530

    payment method details

--- controllers/payment_method.controller.js
+++ controllers/payment_method.controller.js
@@ -10,7 +10,28 @@ class paymentMethodController {
                 res.send({ status: 200, data: paymentMethodSave, message: 'Payment Method has been saved successfully' });
             } else {
                 res.send({ status: 400, data: {}, message: 'Payment Method could not be saved' });
-            }            
+            }
+        } catch (e) {
+            res.send({ status: 500, message: e.message });
+        }
+    };
+
+    async details(req, res) {
+        try {
+            let paymentMethodInfo = await PaymentMethod.findOne();
+            if (!_.isEmpty(paymentMethodInfo) && paymentMethodInfo._id) {
+                res.send({ status: 200, data: paymentMethodInfo, message: 'Payment Method details fetched successfully' });
+            } else {
+                res.send({ status: 400, data: {}, message: 'Payment Method not found' });
+            }
+        } catch (e) {
+            res.send({ status: 500, message: e.message });
+        }
+    };
+
+    async update(req, res) {
+        try {
+            
         } catch (e) {
             res.send({ status: 500, message: e.message });
         }
