6544b418aea2d8ad12aee2e8179e7f9bd31d3712
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-28 12:17:15 +0530

    property add

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -5,7 +5,14 @@ class propertyController {
 
     async add(req, res) {
         try {
-
+            req.body.added_by = req.user._id;
+            let propertyCheck = await Property.create(req.body);
+            if (!_.isEmpty(propertyCheck) && propertyCheck._id) {
+                res.send({ status: 200, data: propertyCheck, message: 'Property has been added successfully' });
+            }
+            else {
+                res.send({ status: 400, data: {}, message: 'Property could not be added' });
+            }
         } catch (e) {
             res.send({ status: 500, message: e.message });
         }
