759f33262798ba520261038f853ab49f922a82f9
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-28 11:49:05 +0530

    property updated

--- repositories/property.repository.js
+++ repositories/property.repository.js
@@ -2,7 +2,17 @@ const Property = require('../models/property.model');
 
 const propertyRepository = {
 
-    
+    updateById: async (data, id) => {
+        try {
+            let propertyUpdate = await Property.findByIdAndUpdate(id, data, { new: true, upsert: true }).exec();
+            if (!propertyUpdate) {
+                return null;
+            }
+            return propertyUpdate;
+        } catch (e) {
+            throw e;
+        }
+    }
 
 }
 
