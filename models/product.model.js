eea6f488c1379ef3167eed0b41119fba2745f6ff
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-28 16:53:48 +0530

    product details

--- models/product.model.js
+++ models/product.model.js
@@ -3,20 +3,10 @@ const Schema = mongoose.Schema;
 const mongooseAggregatePaginate = require('mongoose-aggregate-paginate');
 
 const ProductSchema = new Schema({
-    name: { type: String, index: true, trim: true },
+    title: { type: String, index: true, trim: true },
     description: { type: String, index: true, trim: true },
-    price: { type: Number, trim: true },
-    product_type: { type: String, index: true, trim: true },
-    photo: { type: String, default: '', index: true, trim: true },
-    image_1: { type: String, default: '', index: true, trim: true },
-    image_2: { type: String, default: '', index: true, trim: true },
-    image_3: { type: String, default: '', index: true, trim: true },
-    address: { type: String, default: '', index: true, trim: true },
-    lat: { type: String, trim: true },
-    long: { type: String, trim: true },
-    brand: { type: String, index: true },
-    category_id: { type: Schema.Types.ObjectId, ref: 'service_category', index: true },
-    quantity: { type: Number, trim: true }
+    userId: { type: Schema.Types.ObjectId, ref: 'User', trim: true },
+    status: { type: String, index: true, trim: true }
 }, { timestamps: true, versionKey: false });
 
 ProductSchema.plugin(mongooseAggregatePaginate);
