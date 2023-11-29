9cf6945c09f368ce1ba4dba143ddfe1f3f87ecf8
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-29 18:02:40 +0530

    admin updated

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -13,18 +13,18 @@ class adminController {
     async adminRegister(req, res) {
         try {
             if (!_.has(req.body, 'name')) {
-                res.send({ status: 400, message: 'Name is required' });
+                res.status(400).send({ status: 400, message: 'Name is required' });
             }
             else if (!_.has(req.body, 'email')) {
-                res.send({ status: 400, message: 'Email is required' });
+                res.status(400).send({ status: 400, message: 'Email is required' });
             }
             else if (!_.has(req.body, 'password')) {
-                res.send({ status: 400, message: 'Password is required' });
+                res.status(400).send({ status: 400, message: 'Password is required' });
             }
             else {
                 const adminExist = await Admin.findOne({ email: req.body.email });
                 if (!_.isEmpty(adminExist)) {
-                    res.send({ status: 400, message: 'Admin already exists' });
+                    res.status(400).send({ status: 400, message: 'Admin already exists' });
                 }
                 else {
                     let password = req.body.password;
@@ -32,9 +32,9 @@ class adminController {
                     let saveAdmin = await Admin.create(req.body);
                     if (!_.isEmpty(saveAdmin) && saveAdmin._id) {
                         const token = jwt.sign({ email: saveAdmin.email, id: saveAdmin._id }, process.env.JWTSECERT, { expiresIn: process.env.JWTTIME });
-                        res.send({ status: 200, token: token, data: saveAdmin, msg: 'Admin Registration Successful' });
+                        res.status(200).send({ status: 200, token: token, data: saveAdmin, msg: 'Admin Registration Successful' });
                     } else {
-                        res.send({ status: 400, message: 'Admin Registration Unsuccessful' });
+                        res.status(400).send({ status: 400, message: 'Admin Registration Unsuccessful' });
                     }
                 }
             }
@@ -46,10 +46,10 @@ class adminController {
     async adminLogin(req, res) {
         try {
             if (!_.has(req.body, 'email')) {
-                res.send({ status: 400, message: 'Email is required' });
+                res.status(400).send({ status: 400, message: 'Email is required' });
             }
             else if (!_.has(req.body, 'password')) {
-                res.send({ status: 400, message: 'Password is required' });
+                res.status(400).send({ status: 400, message: 'Password is required' });
             }
             else {
                 let password = req.body.password;
@@ -60,7 +60,7 @@ class adminController {
                         res.status(400).json({ status: 400, message: "Password not matched" });
                     } else {
                         let token = jwt.sign({ email: adminInfo.email, id: adminInfo._id }, process.env.JWTSECERT, { expiresIn: process.env.JWTTIME });
-                        res.send({ status: 200, data: adminInfo, token: token, isLoggedIn: true, message: 'Admin Login Successful' });
+                        res.status(200).send({ status: 200, data: adminInfo, token: token, isLoggedIn: true, message: 'Admin Login Successful' });
                     }
                 } else {
                     res.status(400).json({ status: 400, isLoggedIn: false, message: 'Email not matched!' });
@@ -75,9 +75,9 @@ class adminController {
         try {
             let adminDetails = await adminRepo.getAdminDetails(req);
             if (!_.isEmpty(adminDetails) && adminDetails._id) {
-                res.send({ status: 200, data: adminDetails, message: 'admin profile details fetched successfully' });
+                res.status(200).send({ status: 200, data: adminDetails, message: 'admin profile details fetched successfully' });
             } else {
-                res.send({ status: 400, message: 'Admin not found' });
+                res.status(400).send({ status: 400, message: 'Admin not found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -98,9 +98,9 @@ class adminController {
 
             let updateDetails = await adminRepo.updateById(req.body, req.user._id);
             if (!_.isEmpty(updateDetails) && updateDetails._id) {
-                res.send({ status: 200, data: updateDetails, message: 'Admin details has been updated' });
+                res.status(200).send({ status: 200, data: updateDetails, message: 'Admin details has been updated' });
             } else {
-                res.send({ status: 400, message: 'Admin details could not be updated' });
+                res.status(400).send({ status: 400, message: 'Admin details could not be updated' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -114,10 +114,10 @@ class adminController {
             if (!_.isEmpty(adminInfo) && adminInfo._id) {
                 const payload = { id: admin_id };
                 const token = jwt.sign(payload, process.env.JWTSECERT, { expiresIn: 0 });
-                res.send({ status: 200, isLoggedIn: false, message: 'Logout Successfully' });
+                res.status(200).send({ status: 200, isLoggedIn: false, message: 'Logout Successfully' });
             }
             else {
-                res.send({ status: 400, message: 'Admin not found' });
+                res.status(400).send({ status: 400, message: 'Admin not found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -128,19 +128,19 @@ class adminController {
         try {
             let adminInfo = await Admin.findById(req.user._id);
             if (!bcrypt.compareSync(req.body.currentPassword, adminInfo.password)) {
-                res.send({ status: 400, message: 'Wrong Current Password' });
+                res.status(400).send({ status: 400, message: 'Wrong Current Password' });
             }
             if (!_.isEmpty(adminInfo) && adminInfo._id) {
                 req.body.password = adminInfo.generateHash(req.body.newPassword);
                 let updatePassword = await adminRepo.updateById(req.body, req.user._id);
                 if (!_.isEmpty(updatePassword)) {
-                    res.send({ status: 200, data: updatePassword, message: 'Password has been updated successfully' });
+                    res.status(200).send({ status: 200, data: updatePassword, message: 'Password has been updated successfully' });
                 }
                 else {
-                    res.send({ status: 400, message: 'Password could not be updated' });
+                    res.status(400).send({ status: 400, message: 'Password could not be updated' });
                 }
             } else {
-                res.send({ status: 400, message: 'Admin not found' });
+                res.status(400).send({ status: 400, message: 'Admin not found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -164,9 +164,9 @@ class adminController {
 
             let users = await adminRepo.getUsersList(req);
             if (!_.isEmpty(users)) {
-                res.send({ status: 200, data: users.docs, total: users.total, limit: users.limit, page: users.page, pages: users.pages, message: 'Users list fetched successfully' });
+                res.status(200).send({ status: 200, data: users.docs, total: users.total, limit: users.limit, page: users.page, pages: users.pages, message: 'Users list fetched successfully' });
             } else {
-                res.send({ status: 201, message: 'No user found' });
+                res.status(201).send({ status: 201, message: 'No user found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -178,7 +178,7 @@ class adminController {
         try {
             let user = await adminRepo.getUserInfo({ email: req.body.email });
             if (!_.isEmpty(user) && user._id) {
-                res.status(400).send({ data: {}, message: 'User Already Exists' });
+                res.status(400).send({ status: 400, data: {}, message: 'User Already Exists' });
             }
             else {
                 let password = req.body.password;
@@ -191,10 +191,10 @@ class adminController {
 
                 let saveUser = await adminRepo.save(req.body);
                 if (!_.isEmpty(saveUser) && saveUser._id) {
-                    res.status(200).send({ data: saveUser, message: 'User has been added successfully' });
+                    res.status(200).send({ status: 200, data: saveUser, message: 'User has been added successfully' });
                 }
                 else {
-                    res.status(400).send({ data: {}, message: 'User could not be added' });
+                    res.status(400).send({ status: 400, data: {}, message: 'User could not be added' });
                 }
             }
         } catch (e) {
@@ -208,9 +208,9 @@ class adminController {
             const user_id = new mongoose.Types.ObjectId(req.params.id);
             let userInfo = await userRepo.getUserInfo({ _id: user_id });
             if (!_.isEmpty(userInfo) && userInfo._id) {
-                res.send({ status: 200, data: userInfo, message: 'user details fetched successfully' });
+                res.status(200).send({ status: 200, data: userInfo, message: 'user details fetched successfully' });
             } else {
-                res.send({ status: 400, data: {}, message: 'User not found' });
+                res.status(400).send({ status: 400, data: {}, message: 'User not found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -225,13 +225,13 @@ class adminController {
             if (!_.isEmpty(userInfo) && userInfo._id) {
                 let userDelete = await adminRepo.delete(userId);
                 if (!_.isEmpty(userDelete) && userDelete._id) {
-                    res.send({ status: 200, data: userDelete, message: 'User has been removed successfully' });
+                    res.status(200).send({ status: 200, data: userDelete, message: 'User has been removed successfully' });
                 }
                 else {
-                    res.send({ status: 400, data: {}, message: 'User could not be removed' });
+                    res.status(400).send({ status: 400, data: {}, message: 'User could not be removed' });
                 }
             } else {
-                res.send({ status: 400, data: {}, message: 'User not found!' });
+                res.status(400).send({ status: 400, data: {}, message: 'User not found!' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
