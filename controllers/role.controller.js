f78f4165a794bac927937061828dd9c2bef98686
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-30 18:00:04 +0530

    role updated

--- controllers/role.controller.js
+++ controllers/role.controller.js
@@ -9,14 +9,14 @@ class roleController {
         try {
             let roleCheck = await Role.findOne({ role: req.body.role, isDeleted: false });
             if (!_.isEmpty(roleCheck) && roleCheck._id) {
-                res.send({ status: 201, data: {}, message: 'Role already exists' });
+                res.status(400).send({ status: 400, data: {}, message: 'Role already exists' });
             } else {
                 let roleSave = await Role.create(req.body);
                 if (!_.isEmpty(roleSave) && roleSave._id) {
-                    res.send({ status: 200, data: roleSave, message: 'Role saved successfully' });
+                    res.status(200).send({ status: 200, data: roleSave, message: 'Role saved successfully' });
                 }
                 else {
-                    res.send({ status: 201, data: {}, message: 'Role could not be added' });
+                    res.status(400).send({ status: 400, data: {}, message: 'Role could not be added' });
                 }
             }
         } catch (e) {
@@ -40,9 +40,9 @@ class roleController {
 
             let roles = await roleRepo.getRoles(req);
             if (!_.isEmpty(roles)) {
-                res.send({ status: 200, data: roles.docs, total: roles.total, limit: roles.limit, page: roles.page, pages: roles.pages, message: 'Role list fetched successfully' });
+                res.status(200).send({ status: 200, data: roles.docs, total: roles.total, limit: roles.limit, page: roles.page, pages: roles.pages, message: 'Role list fetched successfully' });
             } else {
-                res.send({ status: 201, data: [], message: 'No role found' });
+                res.status(201).send({ status: 201, data: [], message: 'No role found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -54,9 +54,9 @@ class roleController {
             const roleId = new mongoose.Types.ObjectId(req.params.id);
             let roleInfo = await Role.findOne({ _id: roleId, isDeleted: false });
             if (!_.isEmpty(roleInfo) && roleInfo._id) {
-                res.send({ status: 200, data: roleInfo, message: 'Role details fetched successfully' });
+                res.status(200).send({ status: 200, data: roleInfo, message: 'Role details fetched successfully' });
             } else {
-                res.send({ status: 201, data: {}, message: 'Role not found' });
+                res.status(400).send({ status: 400, data: {}, message: 'Role not found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -70,12 +70,12 @@ class roleController {
             if (!_.isEmpty(roleInfo) && roleInfo._id) {
                 let roleUpdate = await roleRepo.updateById(req.body, roleInfo._id);
                 if (!_.isEmpty(roleUpdate) && roleUpdate._id) {
-                    res.send({ status: 200, data: roleUpdate, message: 'Role has been updated' });
+                    res.status(200).send({ status: 200, data: roleUpdate, message: 'Role has been updated' });
                 } else {
-                    res.send({ status: 201, data: {}, message: 'Role could not be updated' });
+                    res.status(400).send({ status: 400, data: {}, message: 'Role could not be updated' });
                 }
             } else {
-                res.send({ status: 201, message: 'Role not found' });
+                res.status(400).send({ status: 400, message: 'Role not found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -89,12 +89,12 @@ class roleController {
             if (!_.isEmpty(roleDetails) && roleDetails._id) {
                 let roleDelete = await roleRepo.updateById({ isDeleted: true }, roleDetails._id);
                 if (!_.isEmpty(roleDelete) && roleDelete._id) {
-                    res.send({ status: 200, data: roleDelete, message: 'Role has been deleted successfully' });
+                    res.status(200).send({ status: 200, data: roleDelete, message: 'Role has been deleted successfully' });
                 } else {
-                    res.send({ status: 201, message: 'Role could not be updated' });
+                    res.status(400).send({ status: 400, message: 'Role could not be updated' });
                 }
             } else {
-                res.send({ status: 201, message: 'Role not found' });
+                res.status(400).send({ status: 400, message: 'Role not found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
