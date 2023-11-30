59acc529bac1fc4915c0711507fb8fe9483df5f7
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-30 17:08:23 +0530

    product wishlist updated

--- controllers/product_wishlist.controller.js
+++ controllers/product_wishlist.controller.js
@@ -13,7 +13,7 @@ class productWishlistController {
             if (!_.isEmpty(user) && user._id) {
                 let productwishlistAdd = await Wishlist.findOne({ user_id: req.user._id, 'products.product_id': { $in: req.body.product_id } });
                 if (!_.isEmpty(productwishlistAdd) && productwishlistAdd._id) {
-                    res.send({ status: 200, data: {}, message: 'Product is already present in your wishlist' });
+                    res.status(200).send({ status: 200, data: {}, message: 'Product is already present in your wishlist' });
                 }
                 else {
                     let wishlist_exist = await Wishlist.findOne({ user_id: req.user._id });
@@ -24,23 +24,23 @@ class productWishlistController {
                         req.body.products = arr;
                         let wishlistAdd = await Wishlist.create(req.body);
                         if (!_.isEmpty(wishlistAdd) && wishlistAdd._id) {
-                            res.send({ status: 200, data: wishlistAdd, message: 'Product is added to your wishlist' });
+                            res.status(200).send({ status: 200, data: wishlistAdd, message: 'Product is added to your wishlist' });
                         }
                         else {
-                            res.send({ status: 201, data: {}, message: 'Product could not be added to wishlist' });
+                            res.status(201).send({ status: 201, data: {}, message: 'Product could not be added to wishlist' });
                         }
                     }
                     else {
                         let updateWishlist = await productwishlistRepo.updateWishlist({ user_id: req.user._id }, { $push: { 'products': { product_id: req.body.product_id } } });
                         if (!_.isEmpty(updateWishlist)) {
                             let userwishlist = await Wishlist.findOne({ user_id: req.user._id });
-                            res.send({ status: 200, data: userwishlist, message: 'Product is added to your wishlist' });
+                            res.status(200).send({ status: 200, data: userwishlist, message: 'Product is added to your wishlist' });
                         }
                     }
                 }
             }
             else {
-                res.send({ status: 400, data: {}, message: 'User not found' });
+                res.status(400).send({ status: 400, data: {}, message: 'User not found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -52,10 +52,10 @@ class productWishlistController {
         try {
             let userwishlistInfo = await productwishlistRepo.getUserWishlist(req);
             if (!_.isEmpty(userwishlistInfo)) {
-                res.send({ status: 200, data: userwishlistInfo, message: 'User Wishlist fetched successfully' });
+                res.status(200).send({ status: 200, data: userwishlistInfo, message: 'User Wishlist fetched successfully' });
             }
             else {
-                res.send({ status: 400, data: {}, message: 'You have no product in your wishlist' });
+                res.status(400).send({ status: 400, data: {}, message: 'You have no product in your wishlist' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -68,15 +68,15 @@ class productWishlistController {
             let userId = req.user._id;
             let wishlist_exist = await Wishlist.findOne({ user_id: userId, 'products.product_id': { $in: new mongoose.Types.ObjectId(req.body.product_id) } });
             if (_.isEmpty(wishlist_exist)) {
-                res.send({ status: 400, data: {}, message: 'Product is not in your wishlist' });
+                res.status(400).send({ status: 400, data: {}, message: 'Product is not in your wishlist' });
             } else {
                 let updateWishlist = await productwishlistRepo.updateWishlist({ user_id: userId }, { $pull: { 'products': { product_id: req.body.product_id } } });
                 if (!_.isEmpty(updateWishlist)) {
                     let user_wishlist = await Wishlist.findOne({ user_id: userId });
-                    res.send({ status: 200, data: user_wishlist, message: 'Product is removed from your wishlist' });
+                    res.status(200).send({ status: 200, data: user_wishlist, message: 'Product is removed from your wishlist' });
                 }
                 else {
-                    res.send({ status: 400, data: {}, message: 'Product could not be removed from wishlist' });
+                    res.status(400).send({ status: 400, data: {}, message: 'Product could not be removed from wishlist' });
                 }
             }
         } catch (e) {
