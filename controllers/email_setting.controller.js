106be95c475749b5159ac3b9aafd0a7b60ac8568
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-11-30 16:16:55 +0530

    email setting updated

--- controllers/email_setting.controller.js
+++ controllers/email_setting.controller.js
@@ -9,9 +9,9 @@ class emailSettingController {
         try {
             let emailSettingSave = await EmailSetting.create(req.body);
             if (!_.isEmpty(emailSettingSave) && emailSettingSave._id) {
-                res.send({ status: 200, data: emailSettingSave, message: 'Email Setting has been saved successfully' });
+                res.status(200).send({ status: 200, data: emailSettingSave, message: 'Email Setting has been saved successfully' });
             } else {
-                res.send({ status: 400, data: {}, message: 'Email Setting could not be saved' });
+                res.status(400).send({ status: 400, data: {}, message: 'Email Setting could not be saved' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -22,9 +22,9 @@ class emailSettingController {
         try {
             const emailSettingInfo = await EmailSetting.findOne();
             if (!_.isEmpty(emailSettingInfo) && emailSettingInfo._id) {
-                res.send({ status: 200, data: emailSettingInfo, message: 'Email Setting details has been fetched successfully' });
+                res.status(200).send({ status: 200, data: emailSettingInfo, message: 'Email Setting details has been fetched successfully' });
             } else {
-                res.send({ status: 400, data: {}, message: 'Email Setting not found' });
+                res.status(400).send({ status: 400, data: {}, message: 'Email Setting not found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -35,9 +35,9 @@ class emailSettingController {
         try {
             let email_setting_info = await EmailSetting.findOne();
             if (!_.isEmpty(email_setting_info) && email_setting_info._id) {
-                res.send({ status: 200, data: email_setting_info, message: 'Email Setting detail has been fetched' });
+                res.status(200).send({ status: 200, data: email_setting_info, message: 'Email Setting detail has been fetched' });
             } else {
-                res.send({ status: 400, data: {}, message: 'Email Setting not found' });
+                res.status(400).send({ status: 400, data: {}, message: 'Email Setting not found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
@@ -51,13 +51,13 @@ class emailSettingController {
             if (!_.isEmpty(emailSettingInfo) && emailSettingInfo._id) {
                 let email_setting_update = await emailsettingRepo.updateById(req.body, email_setting_id);
                 if (!_.isEmpty(email_setting_update) && email_setting_update._id) {
-                    res.send({ status: 200, data: email_setting_update, message: 'Email Setting has been updated successfully' });
+                    res.status(200).send({ status: 200, data: email_setting_update, message: 'Email Setting has been updated successfully' });
                 }
                 else {
-                    res.send({ status: 400, data: {}, message: 'Email Setting could not be updated' });
+                    res.status(400).send({ status: 400, data: {}, message: 'Email Setting could not be updated' });
                 }
             } else {
-                res.send({ status: 400, data: {}, message: 'Email Setting not found' });
+                res.status(400).send({ status: 400, data: {}, message: 'Email Setting not found' });
             }
         } catch (e) {
             res.send({ status: 500, message: e.message });
