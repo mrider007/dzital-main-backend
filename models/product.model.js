15e47696dfa26424e8f409d4371c07960a070327
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-01 18:09:13 +0530

    product model updated

--- models/product.model.js
+++ models/product.model.js
@@ -6,6 +6,8 @@ const ProductSchema = new Schema({
     title: { type: String, index: true, trim: true },
     description: { type: String, index: true, trim: true },
     userId: { type: Schema.Types.ObjectId, ref: 'User', trim: true },
+    status: { type: String, default: 'Not Approved', enum: ['Approved', 'Not Approved'] },
+    category_id: { type: Schema.Types.ObjectId, ref: 'service_category', index: true, trim: true },
     status: { type: String, default: 'Active', index: true, trim: true }
 }, { timestamps: true, versionKey: false });
 
