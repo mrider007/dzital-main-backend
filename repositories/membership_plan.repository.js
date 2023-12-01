886c32828919017a39d7c0cce855f3f1132a78a8
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-01 18:44:21 +0530

    admin membership plan list updated

--- repositories/membership_plan.repository.js
+++ repositories/membership_plan.repository.js
@@ -36,21 +36,39 @@ const MembershipPlanRepository = {
 
             and_clauses.push({});
 
+            let key = req.body.keyword_search;
+
             if (_.isObject(req.body) && _.has(req.body, 'keyword_search')) {
                 and_clauses.push({
                     $or: [
                         { 'title': { $regex: (req.body.keyword_search).trim(), $options: 'i' } }
                     ]
                 });
+
+                // Check if keyword_search has length greater than 0
+                if (key.length > 0) {
+                    // Disable req.body.page and req.body.limit
+                    req.body.page = undefined;
+                    req.body.limit = undefined;
+                }
             }
 
             conditions['$and'] = and_clauses;
 
             let plans = MembershipPlan.aggregate([
-                { $match: conditions }
+                { $match: conditions },
+                { $sort: { _id: -1 } }
             ]);
 
-            var options = { page: req.body.page, limit: req.body.limit };
+            // Only set options if they are not disabled
+            var options = {};
+            if (req.body.page !== undefined) {
+                options.page = req.body.page;
+            }
+            if (req.body.limit !== undefined) {
+                options.limit = req.body.limit;
+            }
+
             let allPlans = await MembershipPlan.aggregatePaginate(plans, options);
             return allPlans;
         } catch (e) {
