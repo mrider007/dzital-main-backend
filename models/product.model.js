94d1e9f7e2e2054a71576dad613faf9d245841c6
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-02 10:34:27 +0530

    product model updated

--- models/product.model.js
+++ models/product.model.js
@@ -7,8 +7,7 @@ const ProductSchema = new Schema({
     description: { type: String, index: true, trim: true },
     userId: { type: Schema.Types.ObjectId, ref: 'User', trim: true },
     status: { type: String, default: 'Not Approved', enum: ['Approved', 'Not Approved'] },
-    category_id: { type: Schema.Types.ObjectId, ref: 'service_category', index: true, trim: true },
-    status: { type: String, default: 'Active', index: true, trim: true }
+    category_id: { type: Schema.Types.ObjectId, ref: 'service_category', index: true, trim: true }
 }, { timestamps: true, versionKey: false });
 
 ProductSchema.plugin(mongooseAggregatePaginate);
