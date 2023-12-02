21ae7bb2831654051a1a392216ab2aa66ed508a6
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-02 17:09:05 +0530

    property route updated

--- routes/property.routes.js
+++ routes/property.routes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.get('/property/details/:id', request_param.any(), Authentication.Authenticate, propertyController.details);
 router.post('/property/add', request_param.any(), Authentication.Authenticate, propertyController.add);
+router.post('/property/list', request_param.any(), Authentication.Authenticate, propertyController.list);
 
 module.exports = router;
