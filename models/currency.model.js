514a8d01e76b1b0545385f49a3eb4933956cf193
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-05 11:12:23 +0530

    currency model updated

--- models/currency.model.js
+++ models/currency.model.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CurrencySchema = new Schema({
-    currency: { type: String, index: true, trim: true }    
+    currency_name: { type: String, index: true, trim: true },
+    code: { type: String, index: true, trim: true },
+    exchange_rate: { type: Number, default: 1 },
+    is_default: { type: Boolean, default: false, enum: [true, false] },
+    country: { type: String, index: true, trim: true },
+    language: { type: String, index: true, trim: true }
 }, { versionKey: false });
 
 module.exports = mongoose.model('Currency', CurrencySchema);
