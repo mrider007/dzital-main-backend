9361480eebcea23a5cef292a559f33b675635947
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-06 13:12:40 +0530

    currency route updated

--- routes/currency.routes.js
+++ routes/currency.routes.js
@@ -6,6 +6,7 @@ const multer = require('multer');
 const request_param = multer();
 
 router.get('/currency/details/:id', request_param.any(), Authentication.AuthenticateAdmin, currencyController.details);
+router.delete('/currency/delete/:id', request_param.any(), Authentication.AuthenticateAdmin, currencyController.delete);
 router.post('/currency/list', request_param.any(), Authentication.AuthenticateAdmin, currencyController.list);
 router.post('/currency/add', request_param.any(), Authentication.AuthenticateAdmin, currencyController.add);
 router.post('/currency/update/:id', request_param.any(), Authentication.AuthenticateAdmin, currencyController.update);
