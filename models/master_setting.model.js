2cc68b5eed91074e1ada531d77bc95b7bd2e2314
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-12 13:33:30 +0530

    updated

--- models/master_setting.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const MasterSettingSchema = new Schema({
-    currency: { type: String, index: true, trim: true },
-    language: { type: String, index: true, trim: true }
-}, { timestamps: true, versionKey: false });
-
-module.exports = mongoose.model('Master_Setting', MasterSettingSchema);
