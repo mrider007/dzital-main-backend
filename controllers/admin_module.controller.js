3d7eca36c4e79728dce7515af29aa03493b342d5
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-12 18:59:50 +0530

    admin module

---
+++ controllers/admin_module.controller.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const AdminModule = require('../models/admin_module.model');
+
+class adminModuleController {
+    constructor() { }
+
+
+}
+
+module.exports = new adminModuleController();
---
+++ repositories/admin_module.repository.js
@@ -0,0 +1,9 @@
+const AdminModule = require('../models/admin_module.model');
+
+const adminModuleRepository = {
+
+    
+
+}
+
+module.exports = adminModuleRepository;
