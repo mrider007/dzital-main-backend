b0f901ad08abd1ee0318e62ab96a1248bc50f435
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-12 11:35:06 +0530

    cms updated

--- controllers/cms.controller.js
+++ controllers/cms.controller.js
@@ -106,7 +106,6 @@ class cmsController {
             const cmsId = new mongoose.Types.ObjectId(req.params.id);
             let cmsInfo = await CMS.findById(cmsId);
             if (!_.isEmpty(cmsInfo) && cmsInfo._id) {
-                //let cmsStatus = (cmsInfo.status == 'Active') ? 'Inactive' : 'Active';
                 let updateData = await cmsRepo.updateById(req.body, cmsId);
                 if (!_.isEmpty(updateData) && updateData._id) {
                     res.status(200).send({ status: 200, data: updateData, message: 'CMS status has been updated successfully' });
