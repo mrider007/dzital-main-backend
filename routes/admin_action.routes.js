018fb5f919bf3dd364bdacda2a8ea9b2289c3816
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-12 12:28:32 +0530

    admin action route updated

--- routes/admin_action.routes.js
+++ routes/admin_action.routes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const multer = require('multer');
 const request_param = multer();
 
-router.get('/admin/action/list', adminActionController.list);
+router.post('/admin/action/list', request_param.any(), adminActionController.list);
 router.post('/admin/action/add', request_param.any(), adminActionController.add);
-router.post('/admin/action/update/:id', adminActionController.edit);
+router.post('/admin/action/update/:id', request_param.any(), adminActionController.edit);
 
 module.exports = router;
