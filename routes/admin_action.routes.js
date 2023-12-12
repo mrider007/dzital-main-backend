72c65404d67f87e9b4860e4471947abf7cb9b66a
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-12 13:19:21 +0530

    admin action route auth updated

--- routes/admin_action.routes.js
+++ routes/admin_action.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const adminActionController = require('../controllers/admin_action.controller');
+const Authentication = require('../middleware/authentication');
 const router = express.Router();
 const multer = require('multer');
 const request_param = multer();
 
-router.post('/admin/action/add', request_param.any(), adminActionController.add);
-router.post('/admin/action/update/:id', request_param.any(), adminActionController.edit);
-router.post('/admin/action/list', request_param.any(), adminActionController.list);
+router.post('/admin/action/add', request_param.any(), Authentication.AuthenticateAdmin, adminActionController.add);
+router.post('/admin/action/update/:id', request_param.any(), Authentication.AuthenticateAdmin, adminActionController.edit);
+router.post('/admin/action/list', request_param.any(), Authentication.AuthenticateAdmin, adminActionController.list);
 
 module.exports = router;
