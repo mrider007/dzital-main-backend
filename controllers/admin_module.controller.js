60c75bbc09fd3187688eec0106a2b7df8f48528a
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-13 12:11:42 +0530

    admin module add

--- controllers/admin_module.controller.js
+++ controllers/admin_module.controller.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
-const AdminModule = require('../models/admin_module.model');
+const Module = require('../models/admin_module.model');
+const adminModuleRepo = require('../repositories/admin_module.repository');
 
 class adminModuleController {
     constructor() { }
 
+    async add(req, res) {
+        try {
+            req.body.slug = req.body.name.trim().replace(/\s+/g, '-').toLowerCase();
+            let actionAdd = await Module.create(req.body);
+            if (!_.isEmpty(actionAdd) && actionAdd._id) {
+                res.send({ status: 200, data: actionAdd, message: 'Admin Action has been added successfully' });
+            }
+            else {
+                res.send({ status: 400, data: {}, message: 'Admin Action could not be added' });
+            }
+        } catch (e) {
+            res.send({ status: 500, message: e.message });
+        }
+    };
 
 }
 
--- routes/admin_module.routes.js
+++ routes/admin_module.routes.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const adminModuleController = require('../controllers/admin_module.controller');
+const Authentication = require('../middleware/authentication');
 const router = express.Router();
+const multer = require('multer');
+const request_param = multer();
+
+router.post('/admin/module/add', request_param.any(), Authentication.AuthenticateAdmin, adminModuleController.add);
 
 module.exports = router;
