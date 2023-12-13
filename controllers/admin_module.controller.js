ba1a5549c4112efcc83b46c35dbcf1af8eb35e7a
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-13 15:08:16 +0530

    admin module updated

--- controllers/admin_module.controller.js
+++ controllers/admin_module.controller.js
@@ -13,9 +13,9 @@ class adminModuleController {
             }
             else {
                 req.body.slug = req.body.name.trim().replace(/\s+/g, '-').toLowerCase();
-                let actionAdd = await Module.create(req.body);
-                if (!_.isEmpty(actionAdd) && actionAdd._id) {
-                    res.send({ status: 200, data: actionAdd, message: 'Module has been added successfully' });
+                let moduleAdd = await Module.create(req.body);
+                if (!_.isEmpty(moduleAdd) && moduleAdd._id) {
+                    res.send({ status: 200, data: moduleAdd, message: 'Module has been added successfully' });
                 }
                 else {
                     res.send({ status: 400, data: {}, message: 'Module could not be added' });
