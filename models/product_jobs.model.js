84e2c1c00ecd62f2fb0d03d574c3a5acb4fa044c
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2023-12-16 17:46:46 +0530

    product jobs updated

--- models/product_jobs.model.js
+++ models/product_jobs.model.js
@@ -15,10 +15,10 @@ const JobSchema = new mongoose.Schema({
     department: { type: String, index: true },
     user_id: { type: Schema.Types.ObjectId, ref: 'User', index: true },
     job_type: { type: Schema.Types.ObjectId, ref: 'Job_Type', index: true },
+    job_status: { type: String, index: true },
     product_id: { type: Schema.Types.ObjectId, ref: 'Product', index: true },
     category_id: { type: Schema.Types.ObjectId, ref: 'service_category', index: true },
-    date: { type: Date, index: true },
-    status: { type: String, default: 'Open', enum: ['Open', 'Ongoing', 'Completed'] }
+    date: { type: Date, index: true }
 }, { timestamps: true, versionKey: false });
 
 JobSchema.plugin(mongooseAggregatePaginate);
