1432bf64a0bb4b8d995a1af1a9e9d2d414f6930d
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-01-02 15:17:29 +0530

    language add

--- controllers/language.controller.js
+++ controllers/language.controller.js
@@ -3,7 +3,18 @@ const Language = require('../models/language.model');
 class languageController {
     constructor() { }
 
-    
+    async add(req, res) {
+        try {
+            let saveData = await Language.create(req.body);
+            if (!_.isEmpty(saveData) && saveData._id) {
+                res.status(200).send({ status: 200, data: saveData, message: 'Language has been added successfully' });
+            } else {
+                res.status(400).send({ status: 400, data: {}, message: 'Language could not be added' });
+            }
+        } catch (e) {
+            res.status(500).send({ status: 500, message: e.message });
+        }
+    };
 
 }
 
--- routes/language.routes.js
+++ routes/language.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const languageController = require('../controllers/language.controller');
+const Authentication = require('../middleware/authentication');
 const router = express.Router();
 const multer = require('multer');
 const request_param = multer();
 
-
+router.post('/language/add', request_param.any(), Authentication.AuthenticateAdmin, languageController.add);
 
 module.exports = router;
