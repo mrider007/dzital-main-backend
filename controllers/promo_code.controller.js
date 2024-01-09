2a929ec9dbfb07900954aa7219ba7bd74e4a4f33
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-01-09 16:04:30 +0530

    promo code update

--- controllers/promo_code.controller.js
+++ controllers/promo_code.controller.js
@@ -23,7 +23,7 @@ class promocodeController {
         try {
             if (!req.body.page) {
                 req.body.page = 1;
-            }            
+            }
             else {
                 req.body.page = parseInt(req.body.page);
             }
@@ -38,10 +38,10 @@ class promocodeController {
             const promocodes = await promocodeRepo.list(req);
             if (!_.isEmpty(promocodes)) {
                 res.status(200).send({ status: 200, data: promocodes.docs, total: promocodes.total, limit: promocodes.limit, page: promocodes.page, pages: promocodes.pages, message: 'Promocode list has been fetched successfully' });
-            } 
+            }
             else {
                 res.status(201).send({ status: 201, data: [], message: 'No promocode found' });
-            }           
+            }
         } catch (e) {
             res.status(500).send({ status: 500, message: e.message });
         }
@@ -53,6 +53,27 @@ class promocodeController {
             let PromocodeInfo = await Promocode.findOne({ _id: promocode_id });
             if (!_.isEmpty(PromocodeInfo) && PromocodeInfo._id) {
                 res.status(200).send({ status: 200, data: PromocodeInfo, message: 'Promocode details has been fetched successfully' });
+            }
+            else {
+                res.status(400).send({ status: 400, data: {}, message: 'Promocode not found' });
+            }
+        } catch (e) {
+            res.status(500).send({ status: 500, message: e.message });
+        }
+    };
+
+    async update(req, res) {
+        try {
+            const promocode_id = new mongoose.Types.ObjectId(req.params.id);
+            let PromocodeInfo = await Promocode.findOne({ _id: promocode_id });
+            if (!_.isEmpty(PromocodeInfo) && PromocodeInfo._id) {
+                let updateData = await promocodeRepo.updateById(req.body, promocode_id);
+                if (!_.isEmpty(updateData) && updateData._id) {
+                    res.status(200).send({ status: 200, data: updateData, message: 'Promocode has been updated successfully' });
+                }
+                else {
+                    res.status(400).send({ status: 400, data: {}, message: 'Promocode could not be updated' });
+                }
             }
             else {
                 res.status(400).send({ status: 400, data: {}, message: 'Promocode not found' });
--- routes/promo_code.routes.js
+++ routes/promo_code.routes.js
@@ -8,5 +8,6 @@ const request_param = multer();
 router.get('/admin/promo_code/details/:id', Authentication.AuthenticateAdmin, promocodeController.details);
 router.post('/admin/promo_code/add', request_param.any(), Authentication.AuthenticateAdmin, promocodeController.add);
 router.post('/admin/promo_code/list', request_param.any(), Authentication.AuthenticateAdmin, promocodeController.list);
+router.post('/admin/promo_code/update/:id', request_param.any(), Authentication.AuthenticateAdmin, promocodeController.update);
 
 module.exports = router;
