908ef549c45b512166dce43750c8f840cec215d1
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-01-09 13:50:32 +0530

    language schema updated

--- models/language.model.js
+++ models/language.model.js
@@ -6,6 +6,7 @@ const languageSchema = new Schema({
     country_name: { type: String, index: true, trim: true },
     country_code: { type: String, index: true, trim: true },
     language: { type: String, index: true, trim: true },
+    country_id: { type: Schema.Types.ObjectId, ref: 'Country' },
     is_default: { type: Boolean, default: false, enum: [true, false] }   
 }, { timestamps: true, versionKey: false });
 
