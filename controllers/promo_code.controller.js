2ebc15d8a7498e9aff70a3f4054c1e5b13208932
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-01-09 13:22:30 +0530

    promocode list

--- controllers/promo_code.controller.js
+++ controllers/promo_code.controller.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Promocode = require('../models/promo_code.model');
+const promocodeRepo = require('../repositories/promo_code.repository');
 
 class promocodeController {
     constructor() { }
@@ -13,6 +14,34 @@ class promocodeController {
             else {
                 res.status(400).send({ status: 400, message: 'Promocode could not be added' });
             }
+        } catch (e) {
+            res.status(500).send({ status: 500, message: e.message });
+        }
+    };
+
+    async list(req, res) {
+        try {
+            if (!req.body.page) {
+                req.body.page = 1;
+            }            
+            else {
+                req.body.page = parseInt(req.body.page);
+            }
+
+            if (!req.body.limit) {
+                req.body.limit = 10
+            }
+            else {
+                req.body.limit = parseInt(req,body.limit);
+            }
+
+            const promocodes = await promocodeRepo.list(req);
+            if (!_.isEmpty(promocodes)) {
+                res.status(200).send({ status: 200, data: promocodes.docs, total: promocodes.total, limit: promocodes.limit, page: promocodes.page, pages: promocodes.pages, message: 'Promocode list has been fetched successfully' });
+            } 
+            else {
+                res.status(201).send({ status: 201, data: [], message: 'No promocode found' });
+            }           
         } catch (e) {
             res.status(500).send({ status: 500, message: e.message });
         }
--- repositories/promo_code.repository.js
+++ repositories/promo_code.repository.js
@@ -2,6 +2,56 @@ const Promocode = require('../models/promo_code.model');
 
 const promocodeRepository = {
 
+    list: async (req) => {
+        try {
+            var conditions = {};
+            var and_clauses = [];
+
+            and_clauses.push({});
+
+            let key = req.body.keyword_search;
+
+            if (_.isObject(req.body) && _.has(req.body, 'keyword_search')) {
+                and_clauses.push({
+                    $or: [
+                        { 'title': { $regex: (req.body.keyword_search).trim(), $options: 'i' } }
+                    ]
+                });
+
+                // Check if keyword_search has length greater than 0
+                if (key.length > 0) {
+                    // Disable req.body.page and req.body.limit
+                    req.body.page = undefined;
+                    req.body.limit = undefined;
+                }
+            }
+
+            conditions['$and'] = and_clauses;
+
+            let promo_code = Promocode.aggregate([
+                { $match: conditions },
+                { $sort: { _id: -1 } }
+            ]);
+            if (!promo_code) {
+                return null;
+            }
+
+            // Only set options if they are not disabled
+            var options = {};
+            if (req.body.page !== undefined) {
+                options.page = req.body.page;
+            }
+            if (req.body.limit !== undefined) {
+                options.limit = req.body.limit;
+            }
+
+            let allPromocodes = await Promocode.aggregatePaginate(promo_code, options);
+            return allPromocodes;
+        } catch (e) {
+            throw e;
+        }
+    }
+
 }
 
 module.exports = promocodeRepository;
