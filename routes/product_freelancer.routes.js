259b471b14aba74164399916d04bbb1a3e7c2bd8
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-01-09 12:32:33 +0530

    fashion, freelancer product route updated

--- routes/product_freelancer.routes.js
+++ routes/product_freelancer.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const multer = require('multer');
 const request_param = multer();
 
-router.get('/product/freelancer/details/:id', Authentication.Authenticate, freelancerController.details);
-router.post('/product/freelancer/add', request_param.any(), Authentication.Authenticate, freelancerController.add);
+router.get('/product/freelancer/details/:id', freelancerController.details);
+router.post('/product/freelancer/add', request_param.any(), freelancerController.add);
 
 module.exports = router;
