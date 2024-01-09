8e2bac569d7563ae9853c2ea5aad62aa20918de4
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-01-09 12:41:16 +0530

    product real estate route updated

--- routes/product_real_estate.routes.js
+++ routes/product_real_estate.routes.js
@@ -5,8 +5,8 @@ const multer = require('multer');
 const request_param = multer();
 const router = express.Router();
 
-router.get('/property/details/:id', request_param.any(), Authentication.Authenticate, propertyController.details);
-router.post('/property/add', request_param.any(), Authentication.Authenticate, propertyController.add);
-router.post('/property/list', request_param.any(), Authentication.Authenticate, propertyController.list);
+router.get('/property/details/:id', request_param.any(), propertyController.details);
+router.post('/property/add', request_param.any(), propertyController.add);
+router.post('/property/list', request_param.any(), propertyController.list);
 
 module.exports = router;
