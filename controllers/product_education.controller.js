8d07a6a61dcd92be5d887b70deaa562c7a58cf4e
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-02-06 14:00:11 +0530

    product lesson and courses list

--- controllers/product_education.controller.js
+++ controllers/product_education.controller.js
@@ -1,9 +1,36 @@
 const ProductEducation = require('../models/product_education.model');
+const educationRepo = require('../repositories/product_education.repository');
 
 class productEducationController {
     constructor() { }
 
+    async lessonCoursesList(req, res) {
+        try {
+            if (!req.body.page) {
+                req.body.page = 1;
+            }
+            else {
+                req.body.page = parseInt(req.body.page);
+            }
 
+            if (!req.body.limit) {
+                req.body.limit = 10;
+            }
+            else {
+                req.body.limit = parseInt(req.body.limit);
+            }
+
+            const lesson_courses = await educationRepo.list(req);
+            if (!_.isEmpty(lesson_courses)) {
+                res.status(200).send({ status: 200, data: lesson_courses.docs, total: lesson_courses.total, limit: lesson_courses.limit, page: lesson_courses.page, pages: lesson_courses.pages, message: 'Lesson & Courses Products fetched Successfully' });
+            }
+            else {
+                res.status(201).send({ status: 201, data: [], message: 'No Products Found' });
+            }
+        } catch (e) {
+            res.status(500).send({ status: 500, message: e.message });
+        }
+    };
 
 }
 
--- routes/product_education.routes.js
+++ routes/product_education.routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const multer = require('multer');
 const request_param = multer();
 
-
+router.post('/product/lesson-courses/list', request_param.any(), productEducationController.lessonCoursesList);
 
 module.exports = router;
