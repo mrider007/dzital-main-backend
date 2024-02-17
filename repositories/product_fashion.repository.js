c1315e3a7acd18ffde1096a8068da98d0658a3f9
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-02-17 16:51:29 +0530

    product fashion details

--- repositories/product_fashion.repository.js
+++ repositories/product_fashion.repository.js
@@ -74,6 +74,48 @@ const productFashionRepository = {
         } catch (e) {
             throw e;
         }
+    },
+
+    getDetails: async (params) => {
+        try {
+            let product = await ProductFashion.aggregate([
+                { $match: params },
+                {
+                    $lookup: {
+                        from: 'products',
+                        localField: 'product_id',
+                        foreignField: '_id',
+                        as: 'product_details'
+                    }
+                },
+                { $unwind: { path: '$product_details', preserveNullAndEmptyArrays: true } },
+                {
+                    $group: {
+                        _id: '$_id',
+                        title: { $first: '$title' },
+                        description: { $first: '$description' },
+                        price: { $first: '$price' },
+                        product_type: { $first: '$product_type' },
+                        photo: { $first: '$photo' },
+                        image_1: { $first: '$image_1' },
+                        image_2: { $first: '$image_2' },
+                        image_3: { $first: '$image_3' },
+                        brand: { $first: '$brand' },
+                        product_id: { $first: '$product_id' },
+                        category_id: { $first: '$category_id' },
+                        quantity: { $first: '$quantity' },
+                        status: { $first: '$product_details.status' },
+                        createdAt: { $first: '$createdAt' }
+                    }
+                }
+            ]);
+            if (!product) {
+                return null;
+            }
+            return product[0];
+        } catch (e) {
+            throw e;
+        }
     }
 
 }
--- routes/product_fashion.routes.js
+++ routes/product_fashion.routes.js
@@ -6,5 +6,6 @@ const multer = require('multer');
 const request_param = multer();
 
 router.post('/product/fashion/list', request_param.any(), productFashionController.list);
+router.get('/product/fashion/details/:id', productFashionController.details);
 
 module.exports = router;
