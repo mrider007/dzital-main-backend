7b298e7cdf4b689266354bbfed2c572ea1a20f08
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-02-19 11:58:02 +0530

    product lesson and courses model updated

--- models/product_education.model.js
+++ models/product_education.model.js
@@ -7,7 +7,7 @@ const productEducationSchema = new Schema({
     description: { type: String, index: true, trim: true },
     image: { type: String, default: '', index: true, trim: true },
     address: { type: String, default: '', index: true, trim: true },
-    user_id: { type: Schema.Types.ObjectId, ref: 'User', index: true },
+    user_id: { type: Schema.Types.ObjectId, default: null, ref: 'User', index: true },
     category_id: { type: Schema.Types.ObjectId, ref: 'service_category', index: true },
     product_id: { type: Schema.Types.ObjectId, ref: 'Product', index: true }
 }, { timestamps: true, versionKey: false });
