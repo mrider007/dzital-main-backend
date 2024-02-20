c267883037c1893a26bab1e57481fd51cc8a1f5d
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-02-20 16:09:22 +0530

    product schema updated

--- models/product.model.js
+++ models/product.model.js
@@ -9,7 +9,13 @@ const ProductSchema = new Schema({
     status: { type: String, default: 'Not Approved', enum: ['Approved', 'Not Approved'] },
     image: { type: String, default: '', trim: true },
     category_id: { type: Schema.Types.ObjectId, ref: 'service_category', index: true, trim: true },
-    sub_category_id: { type: Schema.Types.ObjectId, ref: 'service_category', default: null, index: true }
+    sub_category_id: { type: Schema.Types.ObjectId, ref: 'service_category', default: null, index: true },
+    bid_now: { type: Boolean, default: false, enum: [true, false] },
+    bid_start_price: { type: Number, default: 0 },
+    bid_increament_value: { type: Number, default: 0 },
+    bid_entry: { type: Number, default: 0 },
+    bid_start_date: { type: Date },
+    bid_end_date: { type: Date }
 }, { timestamps: true, versionKey: false });
 
 ProductSchema.plugin(mongooseAggregatePaginate);
