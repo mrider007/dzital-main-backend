791d352b1c9529b124511c417bd5d39551b3da3a
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-02-20 13:54:54 +0530

    product electronics schema updated

--- models/product_electronics.model.js
+++ models/product_electronics.model.js
@@ -16,6 +16,7 @@ const ProductElectronicsSchema = new Schema({
     long: { type: String, trim: true },
     brand: { type: String, index: true },
     category_id: { type: Schema.Types.ObjectId, ref: 'service_category', index: true },
+    sub_category_id: { type: Schema.Types.ObjectId, ref: 'service_category', default: null, index: true },
     user_id: { type: Schema.Types.ObjectId, default: null, ref: 'User', index: true },
     product_id: { type: Schema.Types.ObjectId, ref: 'Product', index: true },
     quantity: { type: Number, trim: true }
