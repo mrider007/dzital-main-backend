268077d66b4adba56bea00de7d386b0d8db71150
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-02-23 12:04:08 +0530

    admin attribute add

--- routes/attribute.routes.js
+++ routes/attribute.routes.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const attributeController = require('../controllers/attribute.controller');
+const Authentication = require('../middleware/authentication');
 const router = express.Router();
+const multer = require('multer');
+const request_param = multer();
 
+router.post('/attribute/add', request_param.any(), Authentication.AuthenticateAdmin, attributeController.attributeAdd);
 
 module.exports = router;
