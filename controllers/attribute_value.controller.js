6bede5ff02e029b12a2a54f12c636761ce1922f7
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-02-26 14:24:32 +0530

    product attribute value add

--- controllers/attribute_value.controller.js
+++ controllers/attribute_value.controller.js
@@ -4,6 +4,20 @@ const AttributeValue = require('../models/attribute_value.model');
 class attributevalueController {
     constructor() { }
 
+    /** User Product Attribute Value Add */
+    async productattributeValueAdd(req, res) {
+        try {
+            let attributeValueSave = await AttributeValue.create(req.body);
+            if (!_.isEmpty(attributeValueSave)) {
+                res.send({ status: 200, data: attributeValueSave, message: 'Product Attribute Value saved successfully' });
+            }
+            else {
+                res.send({ status: 400, message: 'Product Attribute Value not saved' });
+            }
+        } catch (e) {
+            res.send({ status: 500, message: e.message });
+        }
+    };
 
 }
 
