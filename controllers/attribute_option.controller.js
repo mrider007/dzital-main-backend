9d1691bf3be896d16bee21876eb12d0a4b2adf98
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-02-27 18:30:49 +0530

    attribute option add

--- controllers/attribute_option.controller.js
+++ controllers/attribute_option.controller.js
@@ -4,7 +4,20 @@ const AttributeOption = require('../models/attribute_option.model');
 class attributeOptionController {
     constructor() { }
 
-    
+    /** Admin Attribute Option Add */
+    async attributeOptionAdd(req, res) {
+        try {
+            let attributeOptionSave = await AttributeOption.create(req.body);
+            if (!_.isEmpty(attributeOptionSave) && attributeOptionSave._id) {
+                res.status(200).send({ status: 200, data: attributeOptionSave, message: 'Attribute Option has been added successfully' });
+            }
+            else {
+                res.status(400).send({ status: 400, message: 'Attribute Option not added' });
+            }
+        } catch (e) {
+            res.status(500).send({ status: 500, message: e.message });
+        }
+    };
 
 }
 
--- routes/attribute_option.routes.js
+++ routes/attribute_option.routes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 const multer = require('multer');
 const request_param = multer();
 
+router.post('/attribute/option/add', request_param.any(), Authentication.AuthenticateAdmin, AttributeOptionController.attributeOptionAdd);
+
 module.exports = router;
