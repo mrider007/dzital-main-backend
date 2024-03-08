ad199eaedf858951f421f746219b7cb1bac5af98
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-03-08 17:38:57 +0530

    product job details updated

--- repositories/product_job.repository.js
+++ repositories/product_job.repository.js
@@ -30,6 +30,15 @@ const JobRepository = {
                 { $unwind: { path: '$category_details', preserveNullAndEmptyArrays: true } },
                 {
                     $lookup: {
+                        from: 'service_categories',
+                        localField: 'sub_category_id',
+                        foreignField: '_id',
+                        as: 'sub_category_details'
+                    }
+                },
+                { $unwind: { path: '$sub_category_details', preserveNullAndEmptyArrays: true } },
+                {
+                    $lookup: {
                         from: 'users',
                         localField: 'user_id',
                         foreignField: '_id',
@@ -113,6 +122,8 @@ const JobRepository = {
                         bid_end_date: { $first: '$product_details.bid_end_date' },
                         category_id: { $first: '$category_id' },
                         category_name: { $first: '$category_details.title' },
+                        sub_category_id: { $first: '$sub_category_id' },
+                        sub_category_name: { $first: '$sub_category_details.title' },
                         createdAt: { $first: '$createdAt' },
                         attribute_values: { $first: '$attribute_value_details' }
                     }
