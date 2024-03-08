a735509b845067bf491c3e249b8c9e0719d4c9fd
Sudipta Bhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-03-08 21:56:14 +0530

    admin product update modified (goods of all kinds)

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -622,11 +622,18 @@ class productController {
                             }
                         }
                     }
-                    else {
-                        req.body.photo = goodsInfo.photo;
-                        req.body.image_1 = goodsInfo.image_1;
-                        req.body.image_2 = goodsInfo.image_2;
-                        req.body.image_3 = goodsInfo.image_3;
+
+                    if (_.has(req.body, 'attributeData') && req.body.attributeData.length > 0) {
+                        
+                        let attribute_values = [];
+
+                        for (let x = 0; x < req.body.attributeData.length; x++) {
+
+                            let attributeData = await attributevalueRepo.updateByField({ _id: req.body.attributeData[x]._id }, req.body.attributeData[x]);
+                            if (!_.isEmpty(attributeData)) {
+                                attribute_values.push(attributeData);
+                            }
+                        }
                     }
 
                     let goodsUpdate = await goodsRepo.updateById(req.body, goodsInfo._id);
