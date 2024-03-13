3b83478715d0d82e301f6f99d350a91ad8977381
Sudipta Bhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-03-13 09:02:47 +0530

    admin product details updated (real estate)

--- repositories/product_real_estate.repository.js
+++ repositories/product_real_estate.repository.js
@@ -77,7 +77,7 @@ const propertyRepository = {
                 },
                 {
                     $lookup: {
-                        let: { productId: '$_id' },
+                        let: { productId: '$product_id' },
                         from: "attribute_values",
                         pipeline: [
                             {
