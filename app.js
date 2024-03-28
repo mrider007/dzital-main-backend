a535c5c5d6e45e9f7fa740905dfa105d4ebcd89e
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-03-28 10:50:13 +0530

    app js updated

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const { join, resolve } = require('path');
 const http = require('http');
 _ = require("underscore");
 const dotenv = require("dotenv");
+const path = require('path');
 dotenv.config()
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(cors());
 
