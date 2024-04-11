dcc04c3e1bc93b59cc151d618ad91ceb7e96d3d4
Sudipta Bhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-04-11 18:10:26 +0530

    bid history route updated

--- routes/bid_history.routes.js
+++ routes/bid_history.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const Authentication = require('../middleware/authentication');
+const bid_historyController = require('../controllers/bid_history.controller');
 const request_param = multer();
 
-
+router.post('/product/bid/add', Authentication.Authenticate, request_param.any(), bid_historyController.bidAdd);
+router.post('/product/bid/list', Authentication.Authenticate, bid_historyController.bidlist)
 
 module.exports = router;
