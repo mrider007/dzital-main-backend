cf5dbec5c826b97cf1b9a46a2d2d874f312191f5
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-04-12 16:10:55 +0530

    product electronics uploaded

--- repositories/product_electronics.repository.js
+++ repositories/product_electronics.repository.js
@@ -1,266 +1,128 @@
 const mongoose = require('mongoose');
 const ProductElectronics = require('../models/product_electronics.model');
+const AttributeValue = require('../models/attribute_value.model');
+const electronicsRepo = require('../repositories/product_electronics.repository');
+const productRepo = require('../repositories/product.repository');
+const cloudinary = require('cloudinary');
 
-const productElectronicsRepository = {
+class ProductElectronicsController {
+    constructor() { }
 
-    List: async (req) => {
+    async list(req, res) {
         try {
-            var conditions = {};
-            var and_clauses = [];
-
-            and_clauses.push({ status: 'Approved' });
-
-            if (_.isObject(req.body) && _.has(req.body, 'keyword_search')) {
-                and_clauses.push({
-                    $or: [
-                        { 'title': { $regex: (req.body.keyword_search).trim(), $options: 'i' } }
-                    ]
-                });
+            if (!req.body.page) {
+                req.body.page = 1;
+            } else {
+                req.body.page = parseInt(req.body.page);
             }
 
-            if (_.isObject(req.body) && _.has(req.body, 'category_id')) {
-                and_clauses.push({ 'category_id': new mongoose.Types.ObjectId(req.body.category_id) });
+            if (!req.body.limit) {
+                req.body.limit = 10;
+            } else {
+                req.body.limit = parseInt(req.body.limit);
             }
-
-            conditions['$and'] = and_clauses;
-
-            let products = ProductElectronics.aggregate([
-                {
-                    $lookup: {
-                        from: 'service_categories',
-                        localField: 'category_id',
-                        foreignField: '_id',
-                        as: 'category_details'
-                    }
-                },
-                { $unwind: { path: '$category_details', preserveNullAndEmptyArrays: true } },
-                {
-                    $lookup: {
-                        from: 'products',
-                        localField: 'product_id',
-                        foreignField: '_id',
-                        as: 'product_details'
-                    }
-                },
-                { $unwind: { path: '$product_details', preserveNullAndEmptyArrays: true } },
-                {
-                    $lookup: {
-                        let: { productId: '$product_id' },
-                        from: "attribute_values",
-                        pipeline: [
-                            {
-                                $match: {
-                                    $expr: {
-                                        $and: [
-                                            { $or: [{ $eq: ["$product_id", "$$productId"] }] },
-                                        ]
-                                    }
-                                }
-                            },
-                            {
-                                $lookup: {
-                                    from: "attributes",
-                                    localField: 'attribute_id',
-                                    foreignField: '_id',
-                                    as: "attribute"
-                                }
-                            },
-                            { $unwind: { path: '$attribute', preserveNullAndEmptyArrays: true } },
-                            {
-                                $group: {
-                                    _id: '$_id',
-                                    attribute: { $first: '$attribute.attribute' },
-                                    value: { $first: '$value' },
-                                }
-                            },
-                            { $sort: { _id: 1 } }
-                        ],
-                        as: "attribute_value_details"
-                    }
-                },
-                {
-                    $group: {
-                        _id: '$_id',
-                        title: { $first: '$title' },
-                        description: { $first: '$description' },
-                        status: { $first: '$product_details.status' },
-                        price: { $first: '$price' },
-                        product_type: { $first: '$product_type' },
-                        photo: { $first: '$photo' },
-                        image_1: { $first: '$image_1' },
-                        image_2: { $first: '$image_2' },
-                        image_3: { $first: '$image_3' },
-                        brand: { $first: '$brand' },
-                        product_id: { $first: '$product_id' },
-                        category_id: { $first: '$category_id' },
-                        quantity: { $first: '$quantity' },
-                        attribute_values: { $first: '$attribute_value_details' },
-                        createdAt: { $first: '$createdAt' }
-                    }
-                },
-                { $match: conditions },
-                { $sort: { _id: -1 } }
-            ]);
-            if (!products) {
-                return null;
+            const electronics = await electronicsRepo.List(req);
+            if (!_.isEmpty(electronics)) {
+                res.status(200).send({ status: 200, data: electronics.docs, total: electronics.total, limit: electronics.limit, page: electronics.page, pages: electronics.pages, message: 'Electronic Products fetched successfully' });
             }
-            var options = { page: req.body.page, limit: req.body.limit };
-            let allProducts = await ProductElectronics.aggregatePaginate(products, options);
-            return allProducts;
+            else {
+                res.status(201).send({ status: 201, data: [], message: 'No Products found' });
+            }
         } catch (e) {
-            throw e;
+            res.status(500).send({ status: 500, message: e.message });
         }
-    },
+    };
 
-    updateById: async (data, id) => {
+    async details(req, res) {
         try {
-            let electronicsUpdate = await ProductElectronics.findByIdAndUpdate(id, data, { new: true, upsert: true }).exec();
-            if (!electronicsUpdate) {
-                return null;
+            const electronic_id = new mongoose.Types.ObjectId(req.params.id);
+            let electronicsInfo = await electronicsRepo.getDetails({ _id: electronic_id });
+            if (!_.isEmpty(electronicsInfo) && electronicsInfo._id) {
+                res.status(200).send({ status: 200, data: electronicsInfo, message: 'Electronic product details has been fetched successfully' });
             }
-            return electronicsUpdate;
+            else {
+                res.status(400).send({ status: 400, data: {}, message: 'Product not found' });
+            }
         } catch (e) {
-            throw e;
+            res.status(500).send({ status: 500, message: e.message });
         }
-    },
+    };
 
-    getDetails: async (params) => {
+    async ElectronicsProductsBulkUpdate(req, res) {
         try {
-            let product = await ProductElectronics.aggregate([
-                { $match: params },
-                {
-                    $lookup: {
-                        from: 'service_categories',
-                        localField: 'category_id',
-                        foreignField: '_id',
-                        as: 'category_details'
+            let productsUpdate = await ProductElectronics.updateMany({}, { $set: { 'sub_category_id': null } });
+            res.status(200).send({ status: 200, data: productsUpdate, message: 'Electronics Products Updated Successfully' });
+        } catch (e) {
+            res.status(500).send({ status: 500, message: e.message });
+        }
+    };
+
+    /** User Electronics Product Add */
+    async ElectronicsProductAdd(req, res) {
+        try {
+            if (req.files && req.files.length > 0) {
+
+                var photo, image_1, image_2, image_3;
+
+                for (let i = 0; i < req.files.length; i++) {
+                    const element = req.files[i];
+                    if (element.fieldname === 'photo') {
+                        photo = element.path;
+                        const uploadResultLogo = await cloudinary.v2.uploader.upload(photo);
+                        req.body.photo = uploadResultLogo.secure_url;
                     }
-                },
-                { $unwind: { path: '$category_details', preserveNullAndEmptyArrays: true } },
-                {
-                    $lookup: {
-                        from: 'users',
-                        localField: 'user_id',
-                        foreignField: '_id',
-                        as: 'user_details'
+                    if (element.fieldname === 'image_1') {
+                        image_1 = element.path;
+                        const uploadResultFaviconLogo = await cloudinary.v2.uploader.upload(image_1);
+                        req.body.image_1 = uploadResultFaviconLogo.secure_url;
                     }
-                },
-                { $unwind: { path: '$user_details', preserveNullAndEmptyArrays: true } },
-                {
-                    $lookup: {
-                        from: 'products',
-                        localField: 'product_id',
-                        foreignField: '_id',
-                        as: 'product_details'
+                    if (element.fieldname === 'image_2') {
+                        image_2 = element.path;
+                        const uploadResultFaviconLogo = await cloudinary.v2.uploader.upload(image_2);
+                        req.body.image_2 = uploadResultFaviconLogo.secure_url;
                     }
-                },
-                { $unwind: { path: '$product_details', preserveNullAndEmptyArrays: true } },
-                {
-                    $lookup: {
-                        let: { subcategoryId: '$sub_category_id' },
-                        from: "attributes",
-                        pipeline: [
-                            {
-                                $match: {
-                                    $expr: {
-                                        $and: [
-                                            { $or: [{ $eq: ["$sub_category_id", "$$subcategoryId"] }] },
-                                        ]
-                                    }
-                                }
-                            },
-                            { $sort: { _id: 1 } }
-                        ],
-                        as: "attribute_details"
-                    }
-                },
-                {
-                    $lookup: {
-                        let: { productId: '$product_id' },
-                        from: "attribute_values",
-                        pipeline: [
-                            {
-                                $match: {
-                                    $expr: {
-                                        $and: [
-                                            { $or: [{ $eq: ["$product_id", "$$productId"] }] },
-                                        ]
-                                    }
-                                }
-                            },
-                            {
-                                $lookup: {
-                                    from: "attributes",
-                                    localField: 'attribute_id',
-                                    foreignField: '_id',
-                                    as: "attribute"
-                                }
-                            },
-                            { $unwind: { path: '$attribute', preserveNullAndEmptyArrays: true } },
-                            {
-                                $group: {
-                                    _id: '$_id',
-                                    product_id: { $first: '$product_id' },
-                                    attribute_id: { $first: '$attribute_id' },
-                                    attribute: { $first: '$attribute.attribute' },
-                                    value: { $first: '$value' },
-                                    createdAt: { $first: '$createdAt' },
-                                    updatedAt: { $first: '$updatedAt' }
-                                }
-                            },
-                            { $sort: { _id: 1 } }
-                        ],
-                        as: "attribute_value_details"
-                    }
-                },
-                {
-                    $group: {
-                        _id: '$_id',
-                        title: { $first: '$title' },
-                        description: { $first: '$description' },
-                        photo: { $first: '$photo' },
-                        image_1: { $first: '$image_1' },
-                        image_2: { $first: '$image_2' },
-                        image_3: { $first: '$image_3' },
-                        product_id: { $first: '$product_id' },
-                        category_id: { $first: '$category_id' },
-                        category_name: { $first: '$category_details.title' },
-                        bid_now: { $first: '$product_details.bid_now' },
-                        bid_start_price: { $first: '$product_details.bid_start_price' },
-                        bid_increament_value: { $first: '$product_details.bid_increament_value' },
-                        bid_entry: { $first: '$product_details.bid_entry' },
-                        bid_start_date: { $first: '$product_details.bid_start_date' },
-                        bid_end_date: { $first: '$product_details.bid_end_date' },
-                        status: { $first: '$product_details.status' },
-                        attributes: { $first: '$attribute_details' },
-                        attribute_values: { $first: '$attribute_value_details' },
-                        createdAt: { $first: '$createdAt' }
+                    if (element.fieldname === 'image_3') {
+                        image_3 = element.path;
+                        const uploadResultFaviconLogo = await cloudinary.v2.uploader.upload(image_3);
+                        req.body.image_3 = uploadResultFaviconLogo.secure_url;
                     }
                 }
-            ]);
-            if (!product) {
-                return null;
             }
-            return product[0];
-        } catch (e) {
-            throw e;
-        }
-    },
+            req.body.user_id = req.user._id;
+            let electronicsProductSave = await ProductElectronics.create(req.body);
+            if (!_.isEmpty(electronicsProductSave) && electronicsProductSave._id) {
 
-    delete: async (id) => {
-        try {
-            let electronics = await ProductElectronics.findById(id);
-            if (electronics) {
-                let electronicsDelete = await ProductElectronics.deleteOne({ _id: id }).exec();
-                if (!electronicsDelete) {
-                    return null;
+                let attribute_values = [];
+
+                for (let x = 0; x < req.body.attributeData.length; x++) {
+                    
+                    req.body.attributeData[x].product_id = req.body.product_id;
+
+                    let attributeData = await AttributeValue.create(req.body.attributeData[x]);
+                    if (!_.isEmpty(attributeData)) {
+                        attribute_values.push(attributeData);
+                    }
                 }
-                return electronics;
+                let update_obj = { image: electronicsProductSave.photo }
+                if(req.body.bid_now === true || req.body.bid_now === "true"){
+                    update_obj.bid_now = req.body.bid_now
+                    update_obj.bid_start_price = req.body.bid_start_price
+                    update_obj.bid_increament_value = req.body.bid_increament_value
+                    update_obj.bid_entry = Number(req.body.bid_start_price) + Number(req.body.bid_increament_value)
+                    update_obj.bid_start_date = req.body.bid_start_date
+                    update_obj.bid_end_date = req.body.bid_end_date
+                }
+                let productUpdate = await productRepo.updateProductById(update_obj, electronicsProductSave.product_id);
+                res.status(200).send({ status: 200, data: electronicsProductSave, message: 'Electronics Product Saved Successfully' });
+            }
+            else {
+                res.status(400).send({ status: 400, data: {}, message: 'Electronics Product could not be added' });
             }
         } catch (e) {
-            throw e;
+            res.status(500).send({ status: 500, message: e.message });
         }
-    }
+    };
+
 }
 
-module.exports = productElectronicsRepository;
+module.exports = new ProductElectronicsController();
