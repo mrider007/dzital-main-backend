f86a9e2463312fc1e749fbb7d25cb0f3d475ea19
Sudipta Bhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-04-18 23:05:13 +0530

    product real estate updated

--- repositories/product_real_estate.repository.js
+++ repositories/product_real_estate.repository.js
@@ -229,13 +229,10 @@ const propertyRepository = {
                 })
             }
 
-            // Filter based on sub category
-            let sub_category_id = req.body.sub_category_id
-
-            if (sub_category_id) {
-                and_clauses.push({ 'sub_category_id': new mongoose.Types.ObjectId(sub_category_id) });
+            if (_.isObject(req.body) && _.has(req.body, 'sub_category_id')) {
+                and_clauses.push({ 'sub_category_id': new mongoose.Types.ObjectId(req.body.sub_category_id) });
             }
-            
+
             if (_.isObject(req.body) && _.has(req.body, 'category_id')) {
                 and_clauses.push({ 'category_id': new mongoose.Types.ObjectId(req.body.category_id) });
             }
