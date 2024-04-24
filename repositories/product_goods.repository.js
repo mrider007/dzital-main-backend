bb578caa23255ec57250e1c2b3db6204806e5935
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-04-24 16:53:51 +0530

    product goods of all kinds updated

--- repositories/product_goods.repository.js
+++ repositories/product_goods.repository.js
@@ -452,6 +452,7 @@ const goodsRepository = {
                         product_id: { $first: '$product_id' },
                         category_id: { $first: '$category_id' },
                         category_name: { $first: '$category_details.title' },
+                        category_slug: { $first: '$category_details.slug' },
                         bid_now: { $first: '$product_details.bid_now' },
                         bid_start_price: { $first: '$product_details.bid_start_price' },
                         bid_increament_value: { $first: '$product_details.bid_increament_value' },
