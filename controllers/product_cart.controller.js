2cc17b73486421d7bdc8fcfaf34a6764bd2d675c
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-04-26 12:35:49 +0530

    product cart updated

--- controllers/product_cart.controller.js
+++ controllers/product_cart.controller.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const Cart = require('../models/product_cart.model')
 const Product = require('../models/product.model')
 const ProductCartRepository = require('../repositories/product_cart.repository')
@@ -13,6 +14,8 @@ class productCartController {
                     return res.status(400).send({ status: 400, message: "Invalid Data" });
                 }
 
+                const actQuantity = quantity || 1
+
                 const product = await Product.findById(product_id)
                 if (!product || !product._id) {
                     return res.status(404).send({ status: 404, message: "Product Not Found" })
@@ -21,7 +24,7 @@ class productCartController {
                 let productCartAdd = await Cart.findOne({ user_id: req.user._id, 'items.product_id': { $in: product_id } });
                 if (!_.isEmpty(productCartAdd) && productCartAdd._id) {
                     const index = productCartAdd.items.findIndex(item => item.product_id.toString() === product_id)
-                    productCartAdd.items[index].quantity = productCartAdd.items[index].quantity + quantity
+                    productCartAdd.items[index].quantity = productCartAdd.items[index].quantity + actQuantity
                     productCartAdd.items[index].total_price = price * productCartAdd.items[index].quantity
                     await productCartAdd.save()
                     return res.status(200).send({ message: "Product quantity updated successfully", status: 200, data: productCartAdd })
@@ -31,7 +34,7 @@ class productCartController {
                     if (_.isEmpty(cart_exist)) {
                         let data = { user_id: req.user._id }
                         let arr = [];
-                        arr.push({ "product_id": product_id, "quantity": quantity, "total_price": price * quantity })
+                        arr.push({ "product_id": product_id, "quantity": actQuantity, "total_price": price * actQuantity })
                         data.items = arr;
                         let cartAdd = await Cart.create(data);
                         if (!_.isEmpty(cartAdd) && cartAdd._id) {
@@ -42,7 +45,7 @@ class productCartController {
                         }
                     }
                     else {
-                        let updateCart = await ProductCartRepository.updateCart({ user_id: req.user._id }, { $push: { 'items': { product_id: product_id, quantity: quantity, total_price: price * quantity } } });
+                        let updateCart = await ProductCartRepository.updateCart({ user_id: req.user._id }, { $push: { 'items': { product_id: product_id, quantity: actQuantity, total_price: price * actQuantity } } });
                         if (!_.isEmpty(updateCart)) {
                             let userCart = await Cart.findOne({ user_id: req.user._id });
                             res.status(200).send({ status: 200, data: userCart, message: 'Product is added to your cart' });
@@ -68,12 +71,33 @@ class productCartController {
                 res.status(200).send({ status: 200, data: userCartInfo, message: 'User cart fetched successfully' });
             }
             else {
-                res.status(400).send({ status: 400, data: {}, message: 'You have no items in your cart' });
+                res.status(201).send({ status: 400, data: {}, message: 'You have no items in your cart' });
             }
         } catch (e) {
             res.status(500).send({ status: 500, message: e.message });
         }
     };
+
+    async removeProductCart(req, res) {
+        try {
+            let userId = req.user._id;
+            let cart_exist = await Cart.findOne({ user_id: userId, 'items.product_id': { $in: new mongoose.Types.ObjectId(req.body.product_id) } });
+            if (_.isEmpty(cart_exist)) {
+                res.status(400).send({ status: 400, data: {}, message: 'Product is not in your cart' });
+            } else {
+                let updateCart = await ProductCartRepository.updateCart({ user_id: userId }, { $pull: { 'items': { product_id: req.body.product_id } } });
+                if (!_.isEmpty(updateCart)) {
+                    let user_cart = await Cart.findOne({ user_id: userId });
+                    res.status(200).send({ status: 200, data: user_cart, message: 'Product is removed from your cart' });
+                }
+                else {
+                    res.status(400).send({ status: 400, data: {}, message: 'Product could not be removed from cart' });
+                }
+            }
+        } catch (e) {
+            res.status(500).send({ status: 500, message: e.message });
+        }
+    }
 
 }
 
