a8007af9cd7401027fe507aae9ebadf61efdedbf
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-05-03 12:03:55 +0530

    user order add created

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -4,7 +4,20 @@ const Order = require('../models/order.model');
 class orderController {
     constructor() { }
 
-    
+    async addOrder(req, res) {
+        try {
+            req.body.user_id = req.user._id;
+            let orderData = await Order.create(req.body);
+            if (!_.isEmpty(orderData) && orderData._id) {
+                res.status(200).send({ status: 200, message: 'Order Placed Successfully' });
+            } 
+            else {
+                res.status(400).send({ status: 400, message: 'Order could not be added' });
+            }         
+        } catch (e) {
+            res.status(500).send({ message: e.message });
+        }
+    };
 
 }
 
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const OrderController = require('../controllers/order.controller');
+const Authentication = require('../middleware/authentication');
 const router = express.Router();
+const multer = require('multer');
+const request_param = multer();
+
+router.post('/order-add', Authentication.Authenticate, OrderController.addOrder);
 
 module.exports = router;
