63bae7185c200e98ccc0b9fed19caad4dc39c117
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-05-04 15:00:29 +0530

    cart details created

--- routes/product_cart.routes.js
+++ routes/product_cart.routes.js
@@ -6,10 +6,10 @@ const productCartController = require('../controllers/product_cart.controller');
 const request_param = multer();
 
 router.get('/user/product/cart', Authentication.Authenticate, productCartController.userProductCart);
+router.get('/cart/product/details/:id', Authentication.Authenticate, productCartController.productCartDetails);
 router.post('/user/product/cart/add', request_param.any(), Authentication.Authenticate, productCartController.addToCart);
 router.post('/user/product/cart/remove', Authentication.Authenticate, productCartController.removeProductCart);
 router.post('/user/product/cart/quantity', Authentication.Authenticate, productCartController.updateQuantity);
 router.post('/user/product/cart/clear', Authentication.Authenticate, productCartController.clearCart);
-router.get('/cart/product/details/:id', Authentication.Authenticate, productCartController.productCartDetails);
 
 module.exports = router;
