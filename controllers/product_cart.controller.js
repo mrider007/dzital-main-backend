9749bb77da4bbf3f1396d922c894b14294d13824
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-05-09 15:30:08 +0530

    cart updated

--- controllers/product_cart.controller.js
+++ controllers/product_cart.controller.js
@@ -29,7 +29,9 @@ class productCartController {
                     return res.status(404).send({ status: 404, message: "Product Not Found" })
                 }
 
-                if (product.userId === req.user._id) return res.status(400).send({ message: 'can not add own product into cart', status: 400 })
+                if (product.userId === req.user._id) {
+                    return res.status(400).send({ status: 400, message: 'can not add own product into cart' })
+                }
                     
                 let productCartAdd = await Cart.findOne({ user_id: req.user._id, 'items.product_id': { $in: product_id } });
                 if (!_.isEmpty(productCartAdd) && productCartAdd._id) {
