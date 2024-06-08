c65006a9a04218cd64ad02c4593eb98177a0525b
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-06-08 14:44:18 +0530

    app js updated auth zoom refresh token created

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.get('/', async (req, res) => {
                 'Authorization': `Basic ${Buffer.from(`${process.env.ZOOM_API_KEY}:${process.env.ZOOM_API_SECRET}`).toString('base64')}`
             }
         });
+        console.log('response', response.data);
         res.send(response.data.access_token);
     } catch (error) {
         console.error('Error', error);
@@ -47,59 +48,60 @@ app.get('/', async (req, res) => {
     }
 });
 
-// app.get('/auth/zoom', (req, res) => {
-//     const clientId = process.env.ZOOM_API_KEY;
-//     const redirect_uri = encodeURIComponent(process.env.REDIRECT_URI);
-//     const responseType = 'code';
-//     const authorizationUrl = `https://zoom.us/oauth/authorize?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirect_uri}`;
-//     res.redirect(authorizationUrl);
-// });
+app.get('/auth/zoom', (req, res) => {
+    const clientId = process.env.ZOOM_API_KEY;
+    const redirect_uri = encodeURIComponent(process.env.REDIRECT_URI);
+    const responseType = 'code';
+    const authorizationUrl = `https://zoom.us/oauth/authorize?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirect_uri}`;
+    res.redirect(authorizationUrl);
+});
 
-// app.get('/callback', async (req, res) => {
-//     const code = req.query.code;
-//     if (!code) {
-//         return res.status(400).send('No code provided');
-//     }
-//     try {
-//         const response = await axios.post('https://zoom.us/oauth/token', null, {
-//             params: {
-//                 grant_type: 'authorization_code',
-//                 code,
-//                 redirect_uri: process.env.REDIRECT_URI
-//             }, headers: {
-//                 'Authorization': `Basic ${Buffer.from(`${process.env.ZOOM_API_KEY}:${process.env.ZOOM_API_SECRET}`).toString('base64')}`,
-//                 'Content-Type': 'application/x-www-form-urlencoded'
-//             }
-//         });
-//         res.json(response.data);
-//     } catch (error) {
-//         console.error('Error:', error);
-//         res.send('Error obtaining token');
-//     }
-// });
+app.get('/callback', async (req, res) => {
+    const code = req.query.code;
+    if (!code) {
+        return res.status(400).send('No code provided');
+    }
+    try {
+        const response = await axios.post('https://zoom.us/oauth/token', null, {
+            params: {
+                grant_type: 'authorization_code',
+                code,
+                redirect_uri: process.env.REDIRECT_URI
+            }, headers: {
+                'Authorization': `Basic ${Buffer.from(`${process.env.ZOOM_API_KEY}:${process.env.ZOOM_API_SECRET}`).toString('base64')}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        console.log('response', response);
+        res.send(response.data);
+    } catch (error) {
+        console.error('Error:', error);
+        res.send('Error obtaining token');
+    }
+});
 
-// app.get('/refreshToken', async (req, res) => {
-//     try {
-//         const refresh_token = req.query.refreshToken;
+app.get('/refreshToken', async (req, res) => {
+    try {
+        const refresh_token = req.query.refreshToken;
 
-//         const response = await axios.post('https://zoom.us/oauth/token', null, {
-//             params: {
-//                 grant_type: 'refresh_token',
-//                 refresh_token
-//             },
-//             headers: {
-//                 'Authorization': `Basic ${Buffer.from(`${process.env.ZOOM_API_KEY}:${process.env.ZOOM_API_SECRET}`).toString('base64')}`,
-//                 'Content-Type': 'application/x-www-form-urlencoded'
-//             }
-//         });
+        const response = await axios.post('https://zoom.us/oauth/token', null, {
+            params: {
+                grant_type: 'refresh_token',
+                refresh_token
+            },
+            headers: {
+                'Authorization': `Basic ${Buffer.from(`${process.env.ZOOM_API_KEY}:${process.env.ZOOM_API_SECRET}`).toString('base64')}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
 
-//         res.json(response.data);
+        res.send(response.data);
 
-//     } catch (error) {
-//         console.error('Error', error);
-//         res.send('Error refreshing token')
-//     }
-// });
+    } catch (error) {
+        console.error('Error', error);
+        res.send('Error refreshing token')
+    }
+});
 
 app.use(cors());
 
