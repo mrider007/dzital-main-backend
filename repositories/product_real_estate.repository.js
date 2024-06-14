7eb6e5818c5529e7d4e8e25f62891d543636043a
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-06-14 11:35:10 +0530

    seller details added to product real estate details

--- repositories/product_real_estate.repository.js
+++ repositories/product_real_estate.repository.js
@@ -629,17 +629,6 @@ const propertyRepository = {
                     },
                 },
                 { $unwind: { path: '$seller_details', preserveNullAndEmptyArrays: true } },
-
-                // {
-                //     $lookup: {
-                //         from: 'users',
-                //         localField: 'user_id',
-                //         foreignField: '_id',
-                //         as: 'user_details'
-                //     }
-                // },
-                // { $unwind: { path: '$user_details', preserveNullAndEmptyArrays: true } },
-
                 {
                     $lookup: {
                         from: 'products',
