b932ad158a138f59faebbc3707df38739ad3c88e
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-06-15 15:18:32 +0530

    seller product contact to supplier enquiries list updated

--- repositories/contact_to_supplier.repository.js
+++ repositories/contact_to_supplier.repository.js
@@ -25,6 +25,40 @@ const ContactToProviderRepository = {
             conditions['$and'] = and_clauses;
 
             let enquiryList = ContactToSupplier.aggregate([
+                {
+                    $lookup: {
+                        let: { userId: '$userId' },
+                        from: "users",
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $and: [
+                                            { $eq: ["$_id", "$$userId"] },
+                                        ]
+                                    }
+                                }
+                            }
+                        ],
+                        as: "user_details"
+                    }
+                },
+                { $unwind: { path: '$user_details', preserveNullAndEmptyArrays: true } },
+                {
+                    group: {
+                        _id: '$_id',
+                        userId: { $first: '$userId' },
+                        productId: { $first: '$productId' },
+                        sellerId: { $first: '$sellerId' },
+                        quantity: { $first: '$quantity' },
+                        requirements_details: { $first: '$requirements_details' },
+                        user_name: { $first: '$user_details.name' },
+                        user_image: { $first: '$user_details.image' },
+                        user_mobile: { $first: '$user_details.mobile' },
+                        user_address: { $first: '$user_details.address' },
+                        createdAt: { $first: '$createdAt' }
+                    }
+                },            
                 { $match: conditions },
                 { $sort: { _id: -1 } }
             ]);
@@ -57,7 +91,7 @@ const ContactToProviderRepository = {
             let key = req.body.keyword_search;
 
             if (_.isObject(req.body) && _.has(req.body, 'keyword_search')) {
-                
+
                 // Check if keyword_search has length greater than 0
                 if (key.length > 0) {
                     // Disable req.body.page and req.body.limit
