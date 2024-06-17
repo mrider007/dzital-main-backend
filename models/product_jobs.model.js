09692f9ed6422993fb21c762fcc57d7415a6c78d
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-06-17 11:44:35 +0530

    product jobs schema updated company logo field added

--- models/product_jobs.model.js
+++ models/product_jobs.model.js
@@ -6,6 +6,7 @@ const JobSchema = new mongoose.Schema({
     title: { type: String, index: true, trim: true },
     description: { type: String, index: true },
     image: { type: String, default: '', trim: true },
+    company_logo: { type: String, default: '' },
     user_id: { type: Schema.Types.ObjectId, default: null, ref: 'User', index: true },
     product_id: { type: Schema.Types.ObjectId, ref: 'Product', index: true },
     category_id: { type: Schema.Types.ObjectId, ref: 'service_category', index: true },
