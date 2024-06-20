598a8278e5ad593684ec1ae497488bbf2934cac8
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-06-20 16:48:31 +0530

    stripe subscription create free, cancel, resume created

--- routes/subscription_user.routes.js
+++ routes/subscription_user.routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const Authentication = require('../middleware/authentication');
 const SubscriptionUserController = require('../controllers/subscription_user.controller');
-const router = express.Router()
+const router = express.Router();
+const multer = require('multer');
+const request_param = multer();
 
+router.post('/user/free/subscription/create', request_param.any(), Authentication.Authenticate, SubscriptionUserController.create_free);
 router.post('/user/subscription/list', Authentication.Authenticate, SubscriptionUserController.getAllSubsription);
+router.put('/user/subsription/cancel/:id', Authentication.Authenticate, SubscriptionUserController.cancel_subscription);
+router.put('/user/subsription/resume/:id', Authentication.Authenticate, SubscriptionUserController.resume_subscription);
 
 module.exports = router;
