3c60f5612fb85a144c5ac4fa63b5205bf0987068
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-06-24 15:28:47 +0530

    stripe webhook updated

--- services/stripe-webhook.js
+++ services/stripe-webhook.js
@@ -13,6 +13,13 @@ const stripe_webhook = {
             if (subsData && subsData.created !== subsData.current_period_start) {
                 const subscribedUser = await SubscriptionPayment.findOne({ stripe_subs_id: subsData.id }).sort({ createdAt: -1 })
                 if (!_.isEmpty(subscribedUser) && subscribedUser._id) {
+                    const dateObject = new Date(subsData.current_period_start * 1000);
+                    const dateObject2 = new Date(subscribedUser.current_plan_start)
+
+                    if (dateObject.getTime() === dateObject2.getTime()) {
+                        return
+                    }
+
                     let current_plan_start = new Date(subsData?.current_period_start * 1000)
                     let current_plan_end = new Date(subsData?.current_period_end * 1000)
                     let status = session?.status === 'paid' ? 'Active' : 'Inactive'
