80110d63a8acc08525040752ecc024496d1556b8
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-06-24 12:36:49 +0530

    contact us schema created

---
+++ models/contact_us.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const mongooseAggregatePaginate = require('mongoose-aggregate-paginate');
+
+const ContactUsSchema = new Schema({
+    name: { type: String, trim: true, index: true },
+    email: {
+        type: String,
+        lowercase: true,
+        required: [true, `Email can't be Empty`],
+        match: [/\S+@\S+\.\S+/, 'Email is Invalid'],
+        index: true,
+        trim: true
+    },
+    mobile_number: { type: String, default: '' },
+    message: { type: String, trim: true, index: true }
+}, { timestamps: true, versionKey: false });
+
+ContactUsSchema.plugin(mongooseAggregatePaginate);
+
+module.exports = mongoose.model('Contact_Us', ContactUsSchema);
