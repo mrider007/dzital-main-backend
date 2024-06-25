7a14b8a119860fa97b66bdb94cb94ffcc7d7f110
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-06-25 13:53:02 +0530

    membership plan schema created

---
+++ models/membership_user.model.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const mongooseAggregatePaginate = require('mongoose-aggregate-paginate');
+const Schema = mongoose.Schema;
+
+const Membership_User_Schema = new Schema({
+    membership_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Membership_Plan',
+        index: true,
+        required: [true, 'Membership is Required']
+    },
+    payment_id: {
+        type: String,
+        required: [true, 'Payment Id Required']
+    },
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        index: true,
+        required: [true, 'User Required']
+    },
+    membership_status: {
+        type: String,
+        enum: ['Active', 'Inactive'],
+        required: [true, 'Membership Status Required']
+    },
+    membership_end_date: {
+        type: Date,
+        required: [true, 'Membership End Date Required']
+    },
+    amount: {
+        type: Number,
+        required: [true, 'Amount Required']
+    },
+    payment_status: {
+        type: String,
+        enum: ['Success', 'Failed'],
+        required: [true, 'Payment Status Required']
+    }
+}, { timestamps: true, versionKey: false })
+
+Membership_User_Schema.plugin(mongooseAggregatePaginate);
+
+module.exports = mongoose.model('Membership_User', Membership_User_Schema)
