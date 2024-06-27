f1ffa1177247efe3ced0dd1772a75405d461a35e
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-06-27 18:38:02 +0530

    admin attribute add validation added

--- controllers/attribute.controller.js
+++ controllers/attribute.controller.js
@@ -8,12 +8,20 @@ class attributeController {
     /** Admin Sub Category Attribute Add */
     async attributeAdd(req, res) {
         try {
-            let attributeSave = await Attribute.create(req.body);
-            if (!_.isEmpty(attributeSave) && attributeSave._id) {
-                res.status(200).send({ status: 200, data: attributeSave, message: 'Attribute has been added successfully' });
+            const categoryId = new mongoose.Types.ObjectId(req.body.category_id);
+            const subcategoryId = new mongoose.Types.ObjectId(req.body.sub_category_id);
+            let checkAttribute = await Attribute.find({ attribute: req.body.attribute, category_id: categoryId, sub_category_id: subcategoryId });
+            if (!_.isEmpty(checkAttribute)) {
+                res.status(400).send({ status: 400, message: 'Attribute Already Exists For This Category' });
             }
             else {
-                res.status(400).send({ status: 400, message: 'Attribute not added' });
+                let attributeSave = await Attribute.create(req.body);
+                if (!_.isEmpty(attributeSave) && attributeSave._id) {
+                    res.status(200).send({ status: 200, data: attributeSave, message: 'Attribute has been added successfully' });
+                }
+                else {
+                    res.status(400).send({ status: 400, message: 'Attribute not added' });
+                }
             }
         } catch (e) {
             res.status(500).send({ status: 500, message: e.message });
