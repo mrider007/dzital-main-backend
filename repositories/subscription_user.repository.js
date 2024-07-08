1889beb92b45ad29aba322842199063c4adf37c5
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-08 16:18:42 +0530

    get subscribed user updated

--- repositories/subscription_user.repository.js
+++ repositories/subscription_user.repository.js
@@ -138,7 +138,7 @@ const subscriptionUserRepository = {
 
             conditions['$and'] = and_clauses;
 
-            let SubscribedUser = await SubscriptionUser.aggregate([
+            let pipeline = SubscriptionUser.aggregate([
                 { $match: conditions },
                 {
                     $lookup: {
@@ -174,7 +174,7 @@ const subscriptionUserRepository = {
                     $lookup: {
                         from: 'users',
                         localField: 'user_id',
-                        pipleline: [
+                        pipeline: [
                             {
                                 $group: {
                                     _id: '$_id',
@@ -205,6 +205,16 @@ const subscriptionUserRepository = {
                 },
                 { $sort: { createdAt: -1 } }
             ]);
+
+            var options = {};
+            if (req.body.page !== undefined) {
+                options.page = req.body.page;
+            }
+            if (req.body.limit !== undefined) {
+                options.limit = req.body.limit;
+            }
+
+            let SubscribedUser = await SubscriptionUser.aggregatePaginate(pipeline, options);
             return SubscribedUser;
 
         } catch (e) {
