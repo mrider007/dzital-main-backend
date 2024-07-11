5836a29bc7e5c49b0648e5c7ae02b542dae9e7de
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-11 13:33:19 +0530

    user product view record save modified

--- routes/user_view_record.routes.js
+++ routes/user_view_record.routes.js
@@ -6,6 +6,6 @@ const multer = require('multer');
 const request_param = multer();
 
 router.post('/user/product/view/record', request_param.any(), Authentication.Authenticate, UserProductViewRecordController.saveUserProductViewRecord);
-router.post('/user/product/review/list', request_param.any(), Authentication.Authenticate, UserProductViewRecordController.userProductViewRecordList);
+router.post('/user/product/view/list', request_param.any(), Authentication.Authenticate, UserProductViewRecordController.userProductViewRecordList);
 
 module.exports = router;
