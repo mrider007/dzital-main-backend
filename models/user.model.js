a35dc8660227a4b380de1074e983c90a6b913b26
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-17 10:39:30 +0530

    user schema updated

--- models/user.model.js
+++ models/user.model.js
@@ -30,6 +30,8 @@ const UserSchema = new mongoose.Schema({
     purchased_on: { type: Date, index: true, trim: true },
     status: { type: String, default: 'Active', index: true },
     wallet_amount: { type: Number, default: 0 },
+    user_type: { type: String, index: true, enum: ['seller', 'user'] },
+    commission_percentage: { type: Number, default: 20 },
     expired_on: { type: Date, index: true, trim: true },
     forget_pass_token: { type: String, index: true, trim: true, default: '' },
     register_type: { type: String, default: 'normal', enum: ['normal', 'google', 'facebook'] }
