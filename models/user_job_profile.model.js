ca00cb5e343994f8f26ed583b506e266fe39eaa5
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-17 16:50:59 +0530

    user job profile schema updated (new fields added)

--- models/user_job_profile.model.js
+++ models/user_job_profile.model.js
@@ -4,9 +4,12 @@ const Schema = mongoose.Schema;
 const UserJobProfileSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: [true, 'User Id is Required'], index: true },
     cover_photo: { type: String, default: '' },
-    name: { type: String, default: '' },
-    bio: { type: String, default: '' },
-    about: { type: String, default: '' }
+    about: { type: String, default: '' },
+    skills: { type: Array, default: [] },
+    experience: { type: Array, default: [] },
+    education: { type: Array, default: [] },
+    skills: { type: Array, default: [] },
+    languages: { type: Array, default: [] }
 });
 
 module.exports = mongoose.model('User_Job_Profile', UserJobProfileSchema);
