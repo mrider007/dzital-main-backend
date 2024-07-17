849789f49935d02f102dd9fa1fa127fbf54a5526
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-17 14:01:36 +0530

    job applicants list created

--- repositories/job_apply.repository.js
+++ repositories/job_apply.repository.js
@@ -12,7 +12,7 @@ const JobApplyRepository = {
             conditions['$and'] = and_clauses;
 
             const pipeline = JobApply.aggregate([
-                { $match: conditions },
+                // { $match: conditions },
                 {
                     $lookup: {
                         from: 'users',
@@ -24,19 +24,56 @@ const JobApplyRepository = {
                 { $unwind: { path: '$user_details', preserveNullAndEmptyArrays: true } },
                 {
                     $lookup: {
-                        from: 'product_jobs',
-                        localField: 'job_id',
-                        foreignField: '_id',
-                        as: 'job_details'
+                        let: { job_id: '$job_id' },
+                        from: "product_jobs",
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ["$_id", "$$job_id"]
+                                    }
+                                }
+                            },
+                            {
+                                $lookup: {
+                                    let: { subCategory: '$sub_category_id' },
+                                    from: "service_categories",
+                                    pipeline: [
+                                        {
+                                            $match: {
+                                                $expr: {
+                                                    $eq: ["$_id", "$$subCategory"]
+                                                }
+                                            }
+                                        }
+                                    ],
+                                    as: "sub_category_details"
+                                }
+                            },
+                            { $unwind: { path: '$sub_category_details', preserveNullAndEmptyArrays: true } },
+                            {
+                                $group: {
+                                    _id: '$_id',
+                                    job_title: { $first: '$title' },
+                                    image: { $first: '$image' },
+                                    sub_category_name: { $first: '$sub_category_details.title' },
+                                    sub_category_id: { $first: '$sub_category_id' }
+                                }
+                            },
+                        ],
+                        as: "job_details"
                     }
                 },
-                { $unwind: { path: '$job_details', preserveNullAndEmptyArrays: true } },
+                //{ $unwind: { path: '$job_details', preserveNullAndEmptyArrays: true } },
                 {
                     $group: {
                         _id: "$user_id",
-                        job_applicant: { $first: '$user_details.name' }
+                        job_details: { $first: '$job_details' },
+                        job_applicant: { $first: '$user_details.name' },
+                        job_applicant_email: { $first: '$user_details.email' }
                     }
                 },
+                { $match: conditions },
                 { $sort: { job_applicant: 1 } }
             ])
             var options = { page: req.body.page || 1, limit: req.body.limit || 10 };
