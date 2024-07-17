c319d8d506ef24ae7058c120a497117690b692a1
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-17 15:08:39 +0530

    job applicants list updated

--- repositories/job_apply.repository.js
+++ repositories/job_apply.repository.js
@@ -1,4 +1,5 @@
-const JobApply = require("../models/job_apply.model")
+const JobApply = require("../models/job_apply.model");
+const User = require('../models/user.model');
 
 const JobApplyRepository = {
 
@@ -11,73 +12,132 @@ const JobApplyRepository = {
 
             conditions['$and'] = and_clauses;
 
-            const pipeline = JobApply.aggregate([
-                // { $match: conditions },
+            const pipeline = User.aggregate([
                 {
                     $lookup: {
-                        from: 'users',
-                        localField: 'user_id',
-                        foreignField: '_id',
-                        as: 'user_details'
-                    }
-                },
-                { $unwind: { path: '$user_details', preserveNullAndEmptyArrays: true } },
-                {
-                    $lookup: {
-                        let: { job_id: '$job_id' },
-                        from: "product_jobs",
+                        let: { userId: '$_id' },
+                        from: "job_applies",
                         pipeline: [
                             {
                                 $match: {
                                     $expr: {
-                                        $eq: ["$_id", "$$job_id"]
+                                        $eq: ["$user_id", "$$userId"]
                                     }
                                 }
                             },
                             {
                                 $lookup: {
-                                    let: { subCategory: '$sub_category_id' },
-                                    from: "service_categories",
+                                    let: { tc_margin_id: '$job_id' },
+                                    from: "product_jobs",
                                     pipeline: [
                                         {
                                             $match: {
                                                 $expr: {
-                                                    $eq: ["$_id", "$$subCategory"]
+                                                    $eq: ["$_id", "$$tc_margin_id"]
                                                 }
                                             }
+                                        },
+                                        {
+                                            $lookup: {
+                                                let: { categoryID: '$sub_category_id' },
+                                                from: "service_categories",
+                                                pipeline: [
+                                                    {
+                                                        $match: {
+                                                            $expr: {
+                                                                $eq: ["$_id", "$$categoryID"]
+                                                            }
+                                                        }
+                                                    }
+                                                ],
+                                                as: "sub_category_details"
+                                            }
+                                        },
+                                        { $unwind: { path: '$sub_category_details', preserveNullAndEmptyArrays: true } },
+                                        {
+                                            $addFields: { sub_category_name: '$sub_category_details.title' }
                                         }
                                     ],
-                                    as: "sub_category_details"
+                                    as: "job_details"
                                 }
                             },
-                            { $unwind: { path: '$sub_category_details', preserveNullAndEmptyArrays: true } },
+                            { $unwind: { path: '$job_details', preserveNullAndEmptyArrays: true } },
                             {
                                 $group: {
                                     _id: '$_id',
-                                    job_title: { $first: '$title' },
-                                    image: { $first: '$image' },
-                                    sub_category_name: { $first: '$sub_category_details.title' },
-                                    sub_category_id: { $first: '$sub_category_id' }
+                                    user_id: { $first: '$user_id' },
+                                    job_id: { $first: '$job_id' },
+                                    status: { $first: '$status' },
+                                    cv: { $first: '$cv' },
+                                    job_title: { $first: '$job_details.title' },
+                                    sub_category_id: { $first: '$job_details.sub_category_id' },
+                                    sub_category_name: { $first: '$job_details.sub_category_name' }
                                 }
-                            },
+                            }
                         ],
-                        as: "job_details"
+                        as: "job_info"
                     }
                 },
-                //{ $unwind: { path: '$job_details', preserveNullAndEmptyArrays: true } },
+                // {
+                //     $lookup: {
+                //         let: { userId: '$_id' },
+                //         from: "product_jobs",
+                //         pipeline: [
+                //             {
+                //                 $match: {
+                //                     $expr: {
+                //                         $eq: ["$user_id", "$$userId"]
+                //                     }
+                //                 }
+                //             },
+                //             {
+                //                 $lookup: {
+                //                     let: { subCategory: '$sub_category_id' },
+                //                     from: "service_categories",
+                //                     pipeline: [
+                //                         {
+                //                             $match: {
+                //                                 $expr: {
+                //                                     $eq: ["$_id", "$$subCategory"]
+                //                                 }
+                //                             }
+                //                         }
+                //                     ],
+                //                     as: "sub_category_details"
+                //                 }
+                //             },
+                //             { $unwind: { path: '$sub_category_details', preserveNullAndEmptyArrays: true } },
+                //             {
+                //                 $group: {
+                //                     _id: '$_id',
+                //                     job_title: { $first: '$title' },
+                //                     image: { $first: '$image' },
+                //                     sub_category_name: { $first: '$sub_category_details.title' },
+                //                     sub_category_id: { $first: '$sub_category_id' }
+                //                 }
+                //             },
+                //         ],
+                //         as: "job_details"
+                //     }
+                // },
+                {
+                    $match: {
+                        'job_info': { $ne: [] }
+                    }
+                },
                 {
                     $group: {
-                        _id: "$user_id",
-                        job_details: { $first: '$job_details' },
-                        job_applicant: { $first: '$user_details.name' },
-                        job_applicant_email: { $first: '$user_details.email' }
+                        _id: "$_id",
+                        job_details: { $first: '$job_info' },
+                        job_applicant: { $first: '$name' },
+                        job_applicant_email: { $first: '$email' }
                     }
                 },
                 { $match: conditions },
                 { $sort: { job_applicant: 1 } }
             ])
             var options = { page: req.body.page || 1, limit: req.body.limit || 10 };
-            let allJobSeeker = await JobApply.aggregatePaginate(pipeline, options);
+            let allJobSeeker = await User.aggregatePaginate(pipeline, options);
             return allJobSeeker;
 
         } catch (error) {
