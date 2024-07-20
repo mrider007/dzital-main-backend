c7dbcda1e57e783623b628a21326e87ddd6e2014
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-20 16:23:04 +0530

    order updated

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -11,10 +11,10 @@ class orderController {
             let orderData = await Order.create(req.body);
             if (!_.isEmpty(orderData) && orderData._id) {
                 res.status(200).send({ status: 200, message: 'Order Placed Successfully' });
-            } 
+            }
             else {
                 res.status(400).send({ status: 400, message: 'Order could not be added' });
-            }         
+            }
         } catch (e) {
             res.status(500).send({ message: e.message });
         }
