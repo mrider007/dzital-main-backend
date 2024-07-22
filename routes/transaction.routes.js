29b5ca43e1799a672113fc5997fe885723718202
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-22 17:20:22 +0530

    transaction route updated

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -4,6 +4,7 @@ const Authentication = require('../middleware/authentication');
 const TransactionController = require('../controllers/transaction.controller');
 
 router.post('/transaction/list', Authentication.AuthenticateAdmin, TransactionController.getAll);
+router.post('/seller/transaction/list', Authentication.Authenticate, TransactionController.getAll);
 router.post('/transaction/debit/entry', Authentication.AuthenticateAdmin, TransactionController.debit_entry);
 
 module.exports = router
