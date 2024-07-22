921007a710cce1b71431fb9f667d52f5c7362cae
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-22 15:00:22 +0530

    payout request schema updated (remarks added)

--- models/payout_request.model.js
+++ models/payout_request.model.js
@@ -6,6 +6,7 @@ const PayoutRequestSchema = new Schema({
     user_id: { type: Schema.Types.ObjectId, ref: 'User', index: true },
     amount: { type: Number, index: true, trim: true },
     status: { type: String, enum: ['Requested', 'Approved', 'Not Approved'], default: 'Requested' },
+    remarks: { type: String, default: '' }
 }, { timestamps: true, versionKey: false });
 
 PayoutRequestSchema.plugin(mongooseAggregatePaginate);
