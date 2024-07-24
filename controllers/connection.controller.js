556d95c3c47e249b8e7c788837c1f0badb5b9c13
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-24 15:56:30 +0530

    latest frontend build uploaded

--- controllers/connection.controller.js
+++ controllers/connection.controller.js
@@ -15,10 +15,10 @@ class ConnectionController {
             }
 
             const receiver = new mongoose.Types.ObjectId(req.body.receiverId);
-            let checkConnection = await Connection.findOne({ senderId: sender, receiverId: receiver, status: 'Pending' });
+            let checkConnection = await Connection.findOne({ senderId: sender, receiverId: receiver });
             let checkRequest = await Connection.findOne({ senderId: receiver, receiverId: sender, status: 'Pending' });
-            let checkApproved = await Connection.findOne({ senderId: sender, receiverId: receiver, status: 'Accepted' });
-            let checkRejected = await Connection.findOne({ senderId: sender, receiverId: receiver, status: 'Rejected' });
+            //let checkApproved = await Connection.findOne({ senderId: sender, receiverId: receiver, status: 'Accepted' });
+            //let checkRejected = await Connection.findOne({ senderId: sender, receiverId: receiver, status: 'Rejected' });
             if (!_.isEmpty(checkConnection) || !_.isEmpty(checkRequest) || !_.isEmpty(checkApproved) || !_.isEmpty(checkRejected)) {
                 res.status(400).send({ status: 400, message: 'You Have Already Sent Connection Request' });
             }
