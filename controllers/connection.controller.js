e66caa7586566caef43c8c8bca677066816690c6
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-24 12:20:12 +0530

    user connection request send modified

--- controllers/connection.controller.js
+++ controllers/connection.controller.js
@@ -7,19 +7,25 @@ class ConnectionController {
     /** Send Connection Request to Another User */
     async sendConnectionRequest(req, res) {
         try {
-            const senderId = req.user._id;
+            const sender = req.user._id;
 
             if (!_.has(req.body, 'receiverId')) {
                 res.status(400).send({ status: 400, message: 'Receiver Id is Required' });
             }
 
             const receiver = new mongoose.Types.ObjectId(req.body.receiverId);
-            let checkConnection = await Connection.findOne({ senderId: senderId, receiverId: receiver, status: 'Pending' });
+            let checkConnection = await Connection.findOne({ senderId: sender, receiverId: receiver, status: 'Pending' });
             if (!_.isEmpty(checkConnection)) {
                 res.status(400).send({ status: 400, message: 'You Have Already Sent Connection Request' });
             }
             else {
+                req.body.senderId = sender;
                 const sendRequest = await Connection.create(req.body);
+                if (!_.isEmpty(sendRequest) && sendRequest._id) {
+                    res.status(200).send({ status: 200, data: sendRequest, message: 'Connection Request Sent Successfully' });
+                } else {
+                    res.status(400).send({ status: 400, message: 'Connection Request Could Not Be Sent' });
+                }
             }
         } catch (e) {
             res.status(500).send({ message: e.message });
