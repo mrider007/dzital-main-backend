9d510d95a814d3fb9b040572d096a3145eb54be5
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-26 19:02:31 +0530

    product freelancer updated

--- repositories/product_freelancer.repository.js
+++ repositories/product_freelancer.repository.js
@@ -25,15 +25,6 @@ const freelancerRepository = {
                     }
                 },
                 { $unwind: { path: '$sub_category_details', preserveNullAndEmptyArrays: true } },
-                // {
-                //     $lookup: {
-                //         from: 'users',
-                //         localField: 'user_id',
-                //         foreignField: '_id',
-                //         as: 'user_details'
-                //     }
-                // },
-                // { $unwind: { path: '$user_details', preserveNullAndEmptyArrays: true } },
                 {
                     $lookup: {
                         from: "users",
