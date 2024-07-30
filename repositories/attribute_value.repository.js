9f13413a8a10c69551c8388980d95b141d91e17f
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-30 15:01:43 +0530

    attribute value updated

--- repositories/attribute_value.repository.js
+++ repositories/attribute_value.repository.js
@@ -29,12 +29,11 @@ const attributevalueRepository = {
 
     deleteMany: async (product_id) => {
         try {
-            const data = await AttributeValue.deleteMany({ product_id })
-            return data
+            const data = await AttributeValue.deleteMany({ product_id });
+            return data;
         } catch (e) {
             throw e;
         }
-
     }
 
 }
