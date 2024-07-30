36071fcdbefb2b9f73bc6cfd2ac6f64db88b0acb
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-30 12:21:33 +0530

    user profile details updated

--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -47,10 +47,13 @@ const userRepository = {
             var conditions = {};
             var and_clauses = [];
 
-            and_clauses.push({ '_id': req.user._id });
+            and_clauses.push({});
 
             if (!_.isEmpty(req.query) && _.has(req.query, 'user_id')) {
                 and_clauses.push({ '_id': new mongoose.Types.ObjectId(req.query.user_id) });
+            }
+            else {
+                and_clauses.push({ '_id': req.user._id });
             }
 
             conditions['$and'] = and_clauses;
