ac0378865717bc6768f02d998478d1195ac6baf9
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-07-31 13:06:14 +0530

    product fashion schema updated

--- models/product_fashion.model.js
+++ models/product_fashion.model.js
@@ -13,7 +13,7 @@ const productFashionSchema = new Schema({
     lng: { type: Number, default: 0 },
     address: { type: String, default: '', index: true, trim: true },
     price: { type: Number, default: 0 },
-    color: { type: Array, default: [] },
+    color: { type: String, default: '' },
     size: { type: Array, default: [] },
     category_id: { type: Schema.Types.ObjectId, ref: 'service_category', index: true },
     sub_category_id: { type: Schema.Types.ObjectId, ref: 'service_category', default: null, index: true },
