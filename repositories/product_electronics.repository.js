3ed45bbf5288dd6c3c43b864cffd3fff0d70df52
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-08-01 13:42:40 +0530

    product electronics list updated (ratings added)

--- repositories/product_electronics.repository.js
+++ repositories/product_electronics.repository.js
@@ -346,6 +346,28 @@ const productElectronicsRepository = {
                 },
                 {
                     $lookup: {
+                        let: { productID: '$product_id' },
+                        from: "reviews",
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ["$productId", "$$productID"]
+                                    }
+                                }
+                            }
+                        ],
+                        as: "reviews_list"
+                    }
+                },
+                {
+                    $addFields: {
+                        totalReviews: { $size: '$reviews_list' },
+                        totalRating: { $sum: '$reviews_list.rating' }
+                    }
+                },
+                {
+                    $lookup: {
                         let: { productId: '$product_id' },
                         from: "attribute_values",
                         pipeline: [
@@ -399,10 +421,29 @@ const productElectronicsRepository = {
                         product_id: { $first: '$product_id' },
                         category_id: { $first: '$category_id' },
                         sub_category_id: { $first: "$sub_category_id" },
+                        totalReviews: { $first: '$totalReviews' },
+                        totalRating: { $first: '$totalRating' },
                         quantity: { $first: '$quantity' },
                         attribute_values: { $first: '$attribute_value_details' },
                         createdAt: { $first: '$createdAt' },
                         isWishlist: { $first: '$isWishlist' }
+                    }
+                },
+                {
+                    $addFields: {
+                        ratings: {
+                            $cond: {
+                                if: { $gt: ['$totalReviews', 0] },
+                                then: { $round: [{ $divide: ['$totalRating', '$totalReviews'] }, 2] },
+                                else: 0
+                            }
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        totalReviews: 0,
+                        totalRating: 0
                     }
                 },
                 { $match: conditions },
