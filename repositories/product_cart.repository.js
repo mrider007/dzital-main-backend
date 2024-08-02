7bfc65eaed492c3c307a73b144aad9b0a210a498
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-08-02 13:39:15 +0530

    product cart updated

--- repositories/product_cart.repository.js
+++ repositories/product_cart.repository.js
@@ -61,6 +61,7 @@ const ProductCartRepository = {
                                     category_slug: { $first: '$category_details.slug' },
                                     sub_category_id: { $first: '$sub_category_id' },
                                     category_name: { $first: '$category_details.title' },
+                                    product_price: { $first: '$product_price' },
                                     image: { $first: '$image' },
                                     createdAt: { $first: '$createdAt' },
                                     updatedAt: { $first: '$updatedAt' },
@@ -83,6 +84,7 @@ const ProductCartRepository = {
                             $push: {
                                 item_details: "$items.product",
                                 quantity: "$items.quantity",
+                                base_price: '$items.product_price',
                                 total_price: "$items.total_price"
                             }
                         }
