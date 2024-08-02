ecbf1ed1ebf84f0c65d979ae7ee986f25f9e0a32
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-08-02 14:58:38 +0530

    user route updated

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -37,7 +37,6 @@ router.post('/login', request_param.any(), UserController.login);
 router.post('/social-login', request_param.any(), UserController.socialSignup);
 router.post('/profile/update', uploadFile.any(), Authentication.Authenticate, UserController.updateProfile);
 router.post('/change-password', request_param.any(), Authentication.Authenticate, UserController.changePassword);
-router.post('/social-login', request_param.any(), UserController.socialSignup);
 router.post('/user/bio/address/update', UserController.userBioAddressUpdate);
 router.post('/user/forget-password', request_param.any(), UserController.forgetPassword);
 router.post('/user/new-password', request_param.any(), UserController.newPassword);
