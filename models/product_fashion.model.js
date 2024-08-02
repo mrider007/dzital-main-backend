b89b3914d90e8405e8f427bd8d2587074f9ac18f
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-08-02 12:26:49 +0530

    waist size field added to product fashion

--- models/product_fashion.model.js
+++ models/product_fashion.model.js
@@ -13,6 +13,7 @@ const productFashionSchema = new Schema({
     lng: { type: Number, default: 0 },
     address: { type: String, default: '', index: true, trim: true },
     price: { type: Number, default: 0 },
+    waist_size: { type: Array, default: [] },
     color: { type: String, default: '' },
     size: { type: Array, default: [] },
     category_id: { type: Schema.Types.ObjectId, ref: 'service_category', index: true },
