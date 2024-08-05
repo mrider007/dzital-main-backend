a67d9cb105664a7031f16e94dee092d46a144fe6
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-08-05 12:06:42 +0530

    order schema updated

--- models/order.model.js
+++ models/order.model.js
@@ -3,16 +3,18 @@ const Schema = mongoose.Schema;
 const mongooseAggregatePaginate = require('mongoose-aggregate-paginate');
 
 const orderSchema = new Schema({
+    order_id: { type: String, default: '' },
     items: [
         {
             product_id: { type: Schema.Types.ObjectId, ref: 'Product', index: true },
+            category_id: { type: Schema.Types.ObjectId, ref: 'service_category' },
             quantity: { type: Number, default: 1, },
             total_price: { type: Number, default: 0, }
         }
     ],
     total_amount: { type: Number, default: 0 },
     payment_mode: { type: String, default: '', trim: true },
-    status: { type: String, default: 'Success', enum: ['Success', 'Failed'] },
+    status: { type: String, default: '' },
     order_date: { type: Date, default: Date.now() },
     user_id: { type: Schema.Types.ObjectId, ref: 'User', index: true }
 }, { versionKey: false });
