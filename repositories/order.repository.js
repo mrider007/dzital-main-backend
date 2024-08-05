5a9e3a143c98ec3c8c208cf9d9df9aa60bdbf899
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-08-05 18:42:03 +0530

    order updated

--- repositories/order.repository.js
+++ repositories/order.repository.js
@@ -88,7 +88,7 @@ const orderRepository = {
                         order_id: { $first: '$order_id' },
                         order_date: { $first: '$order_date' },
                         status: { $first: '$status' },
-                        promocode_id: { $first: '$promocode_id' }, 
+                        promocode_id: { $first: '$promocode_id' },
                         discount_amount: { $first: '$discount_amount' },
                         total_amount: { $first: '$total_amount' },
                         payment_mode: { $first: '$payment_mode' },
@@ -105,6 +105,48 @@ const orderRepository = {
                         }
                     }
                 },
+                { $match: conditions },
+                { $sort: { _id: -1 } }
+            ];
+
+            let orders = await Order.aggregate(aggregationPipeline);
+
+            if (!orders) {
+                return null;
+            }
+            return orders;
+        } catch (e) {
+            throw e;
+        }
+    },
+
+    getSellerOrders: async (req) => {
+        try {
+            var conditions = {};
+            var and_clauses = [];
+
+            and_clauses.push({});
+
+            conditions['$and'] = and_clauses;
+
+            const aggregationPipeline = [
+                { $unwind: "$items" },
+                // discount_amount: { $first: '$discount_amount' },
+                //         total_amount: { $first: '$total_amount' },
+                //         payment_mode: { $first: '$payment_mode' },
+                //         items: {
+                //             $push: {
+                //                 title: '$items.product.title',
+                //                 category_slug: '$items.product.category_slug',
+                //                 category_name: '$items.product.category_name',
+                //                 image: '$items.product.image',
+                //                 quantity: "$items.quantity",
+                //                 total_price: "$items.total_price",
+                //                 seller_name: '$items.product.seller_name'
+                //             }
+                //         }
+                //     }
+                // },
                 { $match: conditions },
                 { $sort: { _id: -1 } }
             ];
