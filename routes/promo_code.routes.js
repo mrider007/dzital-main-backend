d321141dd68a9c807c6220ab5670fb83f1b4d78d
SudiptaBhattacharjee <bhattacharjee1997sudipta@gmail.com>
2024-08-06 15:32:49 +0530

    promocode route updated

--- routes/promo_code.routes.js
+++ routes/promo_code.routes.js
@@ -9,6 +9,7 @@ router.get('/admin/promo_code/details/:id', Authentication.AuthenticateAdmin, pr
 router.get('/admin/promo_code/delete/:id', Authentication.AuthenticateAdmin, promocodeController.delete);
 router.post('/admin/promo_code/add', request_param.any(), Authentication.AuthenticateAdmin, promocodeController.add);
 router.post('/admin/promo_code/list', request_param.any(), Authentication.AuthenticateAdmin, promocodeController.list);
+router.post('/promo_code/list', request_param.any(), Authentication.Authenticate, promocodeController.list);
 router.post('/admin/promo_code/update/:id', request_param.any(), Authentication.AuthenticateAdmin, promocodeController.update);
 
 module.exports = router;
