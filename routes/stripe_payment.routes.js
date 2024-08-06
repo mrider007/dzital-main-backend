d29f8f139b058cecb04e5d462906f2cb8db28e99
Harsh Singh <harsh@indiaadmart.com>
2024-08-06 11:34:08 +0530

    stripe payment added for cart

--- routes/stripe_payment.routes.js
+++ routes/stripe_payment.routes.js
@@ -4,9 +4,11 @@ const StripePaymentController = require('../controllers/stripe_payment.controlle
 const router = express.Router()
 
 router.post('/payment/session/create', Authentication.Authenticate, StripePaymentController.create_payment)
+router.post('/checkout/session/create', Authentication.Authenticate, StripePaymentController.checkout_payment)
 router.post('/subscription/session/create', Authentication.Authenticate, StripePaymentController.subscribe_payment)
 router.post('/membership/session/create', Authentication.Authenticate, StripePaymentController.membership_payment)
 router.post('/subscription/session/verify', StripePaymentController.verify_payment)
+router.post('/checkout/session/verify', Authentication.Authenticate, StripePaymentController.verify_checkout_payment)
 router.post('/membership/session/verify', StripePaymentController.membership_payment_verfication)
 
 module.exports = router;
